c88c3f85f99adc763420a133be45e98077f7972d
Thorsten Wißmann <edu@thorsten-wissmann.de>
2011-08-26 02:51:51 +0200

    added a dmenu script

---
+++ scripts/execwith.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# exec a script $2... with settings from rc-file $1
+# useful for various dmenu scripts, e.g.:
+# ./execwith.sh ~/.bash_settings ./dmenu.sh use
+
+source "$1"
+shift
+exec "$@"
+
