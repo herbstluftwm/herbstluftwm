fd6309d0c5cb45cd7199ff8fa68eebf947ffe80f
Thorsten Wißmann <edu@thorsten-wissmann.de>
2011-08-31 03:50:00 +0200

    scripts: added loadstate.sh and savestate.sh

---
+++ scripts/loadstate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+hc=${herbstclient_command:-herbstclient}
+
+# loads layouts for each tag comming from stdin
+# the format is the one created by savestate.sh
+
+# a common usage is:
+# savestate.sh > mystate
+# and sometime later:
+# loadstate.sh < mystate
+
+while read line ; do
+    tag="${line%%: *}"
+    tree="${line#*: }"
+    $hc add "$tag"
+    $hc load "$tag" "$tree"
+done
+
+
---
+++ scripts/savestate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+hc=${herbstclient_command:-herbstclient}
+
+# prints a machine readable format of all tags and its layouts
+# one tag with its layout per line
+
+# a common usage is:
+# savestate.sh > mystate
+# and sometime later:
+# loadstate.sh < mystate
+
+$hc complete 1 use |
+while read tag ; do
+    echo -n "$tag: "
+    $hc dump "$tag"
+done
+
+
