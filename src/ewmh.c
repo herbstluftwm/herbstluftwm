a586e374565d46b57bda3a33e667feb8d068d51e
Thorsten Wißmann <edu@thorsten-wissmann.de>
2011-12-25 16:52:29 +0100

    emwh: added __NET_SUPPORTED

--- src/ewmh.c
+++ src/ewmh.c
@@ -4,11 +4,25 @@
  * See LICENSE for details */
 
 #include "ewmh.h"
+#include "utils.h"
 #include "globals.h"
 
 #include <stdio.h>
 
 void ewmh_init() {
+    /* init ewmh net atoms */
+    struct {
+        int     atom;
+        char*   name;
+    } a2n[] = {
+        { NetSupported,             "_NET_SUPPORTED"            },
+    };
+    for (int i = 0; i < LENGTH(a2n); i++) {
+        g_netatom[a2n[i].atom] = XInternAtom(g_display, a2n[i].name, False);
+    }
+    /* tell which ewmh atoms are supported */
+    XChangeProperty(g_display, g_root, g_netatom[NetSupported], XA_ATOM, 32,
+        PropModeReplace, (unsigned char *) g_netatom, NetLast);
 }
 
 void ewmh_destroy() {
--- src/ewmh.h
+++ src/ewmh.h
@@ -6,6 +6,17 @@
 #ifndef __HERBSTLUFT_EWMH_H_
 #define __HERBSTLUFT_EWMH_H_
 
+#include <X11/Xlib.h>
+#include <X11/Xproto.h>
+#include <X11/Xatom.h>
+
+enum {
+    NetSupported,
+    NetLast
+};
+
+Atom g_netatom[NetLast];
+
 void ewmh_init();
 void ewmh_destroy();
 
