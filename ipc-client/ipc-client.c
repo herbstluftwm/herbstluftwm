4955ffcb6f52dc72b7b2d073b7f7633247922459
Thorsten Wißmann <edu@thorsten-wissmann.de>
2012-08-26 15:51:53 +0200

    Extract parts of ipc-client.c to ipc-client/main.c

--- ipc-client/ipc-client.c
+++ ipc-client/ipc-client.c
@@ -6,6 +6,7 @@
 #include "../src/ipc-protocol.h"
 #include "../src/utils.h"
 #include "../src/globals.h"
+#include "ipc-client.h"
 
 // standard
 #include <stdio.h>
@@ -13,8 +14,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <unistd.h>
-#include <getopt.h>
-#include <signal.h>
 #include <regex.h>
 #include <assert.h>
 
@@ -25,29 +24,17 @@
 #include <X11/Xatom.h>
 
 
-int send_command(int argc, char* argv[]);
-void print_help(char* command);
-
-
 Display* g_display;
-Window root;
-int g_ensure_newline = 1; // if set, output ends with an newline
-int g_wait_for_hook = 0; // if set, do not execute command but wait
-int g_hook_count = 1; // count of hooks to wait for, 0 means: forever
 regex_t* g_hook_regex = NULL;
+int g_ensure_newline = 1; // if set, output ends with an newline
 int g_hook_regex_count = 0;
 bool g_quiet = false;
+int g_hook_count = 1; // count of hooks to wait for, 0 means: forever
+
 
 void init_hook_regex(int argc, char* argv[]);
 void destroy_hook_regex();
 Window get_hook_window();
-
-static void quit_herbstclient(int signal) {
-    // TODO: better solution to quit x connection more softly?
-    fprintf(stderr, "interrupted by signal %d\n", signal);
-    destroy_hook_regex();
-    exit(EXIT_FAILURE);
-}
 
 int send_command(int argc, char* argv[]) {
     // check for running window manager instance
@@ -175,10 +162,6 @@ Window get_hook_window() {
 }
 
 int wait_for_hook(int argc, char* argv[]) {
-    // install signals
-    signal(SIGTERM, quit_herbstclient);
-    signal(SIGINT, quit_herbstclient);
-    signal(SIGQUIT, quit_herbstclient);
     init_hook_regex(argc, argv);
     // get window to listen at
     Window win = get_hook_window();
@@ -256,90 +239,3 @@ int wait_for_hook(int argc, char* argv[]) {
     return 0;
 }
 
-void print_help(char* command) {
-    // Eventually replace this and the option parsing with some fancy macro
-    // based thing? Is the cost of maintainence really that high?
-
-    fprintf(stdout,
-        "Usage: %s [OPTIONS] COMMAND [ARGS ...]\n"
-        "       %s [OPTIONS] [--wait|--idle] [FILTER ...]\n",
-        command, command);
-
-    char* help_string =
-        "Send a COMMAND with optional arguments ARGS to a running "
-        "herbstluftwm instance.\n\n"
-        "Options:\n"
-        "\t-n, --no-newline: Do not print a newline if output does not end "
-            "with a newline.\n"
-        "\t-i, --idle: Wait for hooks instead of executing commands.\n"
-        "\t-w, --wait: Same as --idle but exit after first --count hooks.\n"
-        "\t-c, --count COUNT: Let --wait exit after COUNT hooks were "
-            "received and printed. The default of COUNT is 1.\n"
-        "\t-q, --quiet: Do not print error messages if herbstclient cannot "
-            "connect to the running herbstluftwm instance.\n"
-        "\t-h, --help: Print this help."
-        "\n"
-        "See the man page (herbstclient(1)) for more details.\n";
-    fputs(help_string, stdout);
-}
-
-int main(int argc, char* argv[]) {
-    static struct option long_options[] = {
-        {"no-newline", 0, 0, 'n'},
-        {"wait", 0, 0, 'w'},
-        {"count", 1, 0, 'c'},
-        {"idle", 0, 0, 'i'},
-        {"quiet", 0, 0, 'q'},
-        {"help", 0, 0, 'h'},
-        {0, 0, 0, 0}
-    };
-    // parse options
-    while (1) {
-        int option_index = 0;
-        int c = getopt_long(argc, argv, "+nwc:iqh", long_options, &option_index);
-        if (c == -1) break;
-        switch (c) {
-            case 'i':
-                g_hook_count = 0;
-                g_wait_for_hook = 1;
-                break;
-            case 'c':
-                g_hook_count = atoi(optarg);
-                printf("setting to  %s\n", optarg);
-                break;
-            case 'w':
-                g_wait_for_hook = 1;
-                break;
-            case 'n':
-                g_ensure_newline = 0;
-                break;
-            case 'q':
-                g_quiet = true;
-                break;
-            case 'h':
-                print_help(argv[0]);
-                exit(EXIT_SUCCESS);
-            default:
-                fprintf(stderr, "unknown option `%s'\n", argv[optind]);
-                exit(EXIT_FAILURE);
-        }
-    }
-    int arg_index = optind; // index of the first-non-option argument
-    // do communication
-    g_display = XOpenDisplay(NULL);
-    if (!g_display) {
-        if (!g_quiet) {
-            fprintf(stderr, "Cannot open display\n");
-        }
-        return EXIT_FAILURE;
-    }
-    root = DefaultRootWindow(g_display);
-    int command_status;
-    if (g_wait_for_hook == 1) {
-        command_status = wait_for_hook(argc-arg_index, argv+arg_index);
-    } else {
-        command_status = send_command(argc-arg_index, argv+arg_index);
-    }
-    XCloseDisplay(g_display);
-    return command_status;
-}
---
+++ ipc-client/main.c
@@ -0,0 +1,117 @@
+/** Copyright 2011-2012 Thorsten Wißmann. All rights reserved.
+ *
+ * This software is licensed under the "Simplified BSD License".
+ * See LICENSE for details */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <signal.h>
+
+#include "ipc-client.h"
+#include "../src/globals.h"
+
+void print_help(char* command);
+
+int g_wait_for_hook = 0; // if set, do not execute command but wait
+
+static void quit_herbstclient(int signal) {
+    // TODO: better solution to quit x connection more softly?
+    fprintf(stderr, "interrupted by signal %d\n", signal);
+    destroy_hook_regex();
+    exit(EXIT_FAILURE);
+}
+
+void print_help(char* command) {
+    // Eventually replace this and the option parsing with some fancy macro
+    // based thing? Is the cost of maintainence really that high?
+
+    fprintf(stdout,
+        "Usage: %s [OPTIONS] COMMAND [ARGS ...]\n"
+        "       %s [OPTIONS] [--wait|--idle] [FILTER ...]\n",
+        command, command);
+
+    char* help_string =
+        "Send a COMMAND with optional arguments ARGS to a running "
+        "herbstluftwm instance.\n\n"
+        "Options:\n"
+        "\t-n, --no-newline: Do not print a newline if output does not end "
+            "with a newline.\n"
+        "\t-i, --idle: Wait for hooks instead of executing commands.\n"
+        "\t-w, --wait: Same as --idle but exit after first --count hooks.\n"
+        "\t-c, --count COUNT: Let --wait exit after COUNT hooks were "
+            "received and printed. The default of COUNT is 1.\n"
+        "\t-q, --quiet: Do not print error messages if herbstclient cannot "
+            "connect to the running herbstluftwm instance.\n"
+        "\t-h, --help: Print this help."
+        "\n"
+        "See the man page (herbstclient(1)) for more details.\n";
+    fputs(help_string, stdout);
+}
+
+int main(int argc, char* argv[]) {
+    static struct option long_options[] = {
+        {"no-newline", 0, 0, 'n'},
+        {"wait", 0, 0, 'w'},
+        {"count", 1, 0, 'c'},
+        {"idle", 0, 0, 'i'},
+        {"quiet", 0, 0, 'q'},
+        {"help", 0, 0, 'h'},
+        {0, 0, 0, 0}
+    };
+    // parse options
+    while (1) {
+        int option_index = 0;
+        int c = getopt_long(argc, argv, "+nwc:iqh", long_options, &option_index);
+        if (c == -1) break;
+        switch (c) {
+            case 'i':
+                g_hook_count = 0;
+                g_wait_for_hook = 1;
+                break;
+            case 'c':
+                g_hook_count = atoi(optarg);
+                printf("setting to  %s\n", optarg);
+                break;
+            case 'w':
+                g_wait_for_hook = 1;
+                break;
+            case 'n':
+                g_ensure_newline = 0;
+                break;
+            case 'q':
+                g_quiet = true;
+                break;
+            case 'h':
+                print_help(argv[0]);
+                exit(EXIT_SUCCESS);
+            default:
+                fprintf(stderr, "unknown option `%s'\n", argv[optind]);
+                exit(EXIT_FAILURE);
+        }
+    }
+    int arg_index = optind; // index of the first-non-option argument
+    // do communication
+    g_display = XOpenDisplay(NULL);
+    if (!g_display) {
+        if (!g_quiet) {
+            fprintf(stderr, "Cannot open display\n");
+        }
+        return EXIT_FAILURE;
+    }
+    root = DefaultRootWindow(g_display);
+    int command_status;
+    if (g_wait_for_hook == 1) {
+        // install signals
+        signal(SIGTERM, quit_herbstclient);
+        signal(SIGINT, quit_herbstclient);
+        signal(SIGQUIT, quit_herbstclient);
+        command_status = wait_for_hook(argc-arg_index, argv+arg_index);
+    } else {
+        command_status = send_command(argc-arg_index, argv+arg_index);
+    }
+    XCloseDisplay(g_display);
+    return command_status;
+}
+
