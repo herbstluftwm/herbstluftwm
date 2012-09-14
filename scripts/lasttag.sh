97702235d28fec5138f01e068b02c4cd87b67cbc
Thorsten Wißmann <edu@thorsten-wissmann.de>
2012-09-14 17:26:11 +0200

    Add scripts/lasttag.sh

---
+++ scripts/lasttag.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# usage: start this script in anywhere your autostart (but *after* the
+# emit_hook reload line)
+
+# to switch to the last tag, call: herbstclient emit_hook goto_last_tag
+# or bind it: herbstclient keybind Mod1-Escape emit_hook goto_last_tag
+
+herbstclient --idle '(tag_changed|goto_last_tag|reload)' \
+    | while read line ; do
+        ARGS=( $line )
+        case ${ARGS[0]} in
+            tag_changed)
+                LASTTAG="$TAG"
+                TAG=${ARGS[1]}
+                ;;
+            goto_last_tag)
+                ! [ -z "$LASTTAG" ] && herbstclient use "$LASTTAG"
+                ;;
+            reload)
+                exit
+                ;;
+        esac
+    done
+
+
+
