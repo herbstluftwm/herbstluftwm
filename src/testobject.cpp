9585f2bc280ff8e447ecfb010e0c8f2a0f969e41
ypnos <git@lanrules.de>
2014-12-30 17:13:59 +0100

    more testing

--- src/testobject.cpp
+++ src/testobject.cpp
@@ -6,13 +6,16 @@ void test_object_system()
 {
     auto hook = std::make_shared<herbstluft::Hook>("tester.precious.name");
     auto hookII = std::make_shared<herbstluft::Hook>("tester.precious.bar");
+    auto hookIII = std::make_shared<herbstluft::Hook>("tester.precious.sweets.name");
     auto tester = herbstluft::TestObject::tester();
     hook->init(hook, tester);
     hookII->init(hookII, tester);
+    hookIII->init(hookIII, tester);
     tester->print("| ");
     auto wat = std::dynamic_pointer_cast<herbstluft::TestObjectII>(
                    tester->children().begin()->second);
     wat->do_stuff();
+    tester->do_stuff();
 }
 
 TestObject::TestObject()
@@ -45,7 +48,6 @@ std::string TestObject::read(const std::string &attr) const
 
 void TestObject::do_stuff()
 {
-
 }
 
 TestObjectII::TestObjectII(const std::string &name)
@@ -62,11 +64,21 @@ void TestObjectII::init(std::weak_ptr<Object> self)
 
     wireAttributes({ &foo_, &bar_, &checker_ });
     wireActions({ &killer_ });
+
+    auto foo = std::make_shared<Object>("sweets");
+    foo->init(foo);
+    children_.insert(std::make_pair(foo->name(), foo));
+    auto fooII = std::make_shared<Object>("cake");
+    fooII->init(fooII);
+    children_.insert(std::make_pair(fooII->name(), fooII));
 }
 
 void TestObjectII::do_stuff()
 {
     bar_ = false;
+
+    children_.erase("sweets");
+    notifyHooks();
 }
 
 }
