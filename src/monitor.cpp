herbstluftwm/herbstluftwm

commit 1c84e669cd8f5a1cf3f7b4ad84a73b9685226d1b

Make Color, Rectangle, RectangleVec classes

src/monitor.cpp
@@ -42,7 +42,7 @@ typedef struct RectList {
 } RectList;
 
 static RectList* reclist_insert_disjoint(RectList* head, RectList* mt);
-static RectList* disjoin_rects(Rectangle* buf, size_t count);
+static RectList* disjoin_rects(const RectangleVec &buf);
 
 void monitor_init() {
     g_monitors_locked = &(settings_find("monitors_locked")->value.i);
@@ -268,12 +268,12 @@ static int rectlist_length(RectList* head) {
     return rectlist_length_acc(head, 0);
 }
 
-static RectList* disjoin_rects(Rectangle* buf, size_t count) {
+static RectList* disjoin_rects(const RectangleVec &buf) {
     RectList* cur;
     struct RectList* rects = NULL;
-    for (unsigned int i = 0; i < count; i++) {
+    for (auto& rect : buf) {
         cur = g_new0(RectList, 1);
-        cur->rect = buf[i];
+        cur->rect = rect;
         rects = reclist_insert_disjoint(rects, cur);
     }
     return rects;
@@ -285,19 +285,18 @@ int disjoin_rects_command(int argc, char** argv, GString* output) {
     if (argc < 1) {
         return HERBST_NEED_MORE_ARGS;
     }
-    Rectangle* buf = g_new(Rectangle, argc);
+    RectangleVec buf(argc);
     for (int i = 0; i < argc; i++) {
-        buf[i] = parse_rectangle(argv[i]);
+        buf[i] = Rectangle::fromStr(argv[i]);
     }
 
-    RectList* rects = disjoin_rects(buf, argc);
+    RectList* rects = disjoin_rects(buf);
     for (RectList* cur = rects; cur; cur = cur->next) {
-        Rectangle r = cur->rect;
+        Rectangle &r = cur->rect;
         g_string_append_printf(output, "%dx%d%+d%+d\n",
             r.width, r.height, r.x, r.y);
     }
     rectlist_free(rects);
-    g_free(buf);
     return 0;
 }
 
@@ -306,12 +305,12 @@ int set_monitor_rects_command(int argc, char** argv, GString* output) {
     if (argc < 1) {
         return HERBST_NEED_MORE_ARGS;
     }
-    Rectangle* templates = g_new0(Rectangle, argc);
+    RectangleVec templates(argc);
     for (int i = 0; i < argc; i++) {
-        templates[i] = parse_rectangle(argv[i]);
+        templates[i] = Rectangle::fromStr(argv[i]);
     }
-    int status = set_monitor_rects(templates, argc);
-    g_free(templates);
+    int status = set_monitor_rects(templates);
+
     if (status == HERBST_TAG_IN_USE) {
         g_string_append_printf(output,
             "%s: There are not enough free tags\n", argv[0]);
@@ -322,18 +321,18 @@ int set_monitor_rects_command(int argc, char** argv, GString* output) {
     return status;
 }
 
-int set_monitor_rects(Rectangle* templates, size_t count) {
-    if (count < 1) {
+int set_monitor_rects(const RectangleVec &templates) {
+    if (templates.empty()) {
         return HERBST_INVALID_ARGUMENT;
     }
     HSTag* tag = NULL;
     int i;
-    for (i = 0; i < MIN(count, g_monitors->len); i++) {
+    for (i = 0; i < std::min(templates.size(), (size_t)g_monitors->len); i++) {
         HSMonitor* m = monitor_with_index(i);
         m->rect = templates[i];
     }
     // add additional monitors
-    for (; i < count; i++) {
+    for (; i < templates.size(); i++) {
         tag = find_unused_tag();
         if (!tag) {
             return HERBST_TAG_IN_USE;
@@ -491,7 +490,7 @@ int add_monitor_command(int argc, char** argv, GString* output) {
     if (argc < 2) {
         return HERBST_NEED_MORE_ARGS;
     }
-    auto rect = parse_rectangle(argv[1]);
+    auto rect = Rectangle::fromStr(argv[1]);
     HSTag* tag = NULL;
     char* name = NULL;
     if (argc == 2 || !strcmp("", argv[2])) {
@@ -620,7 +619,7 @@ int move_monitor_command(int argc, char** argv, GString* output) {
             "%s: Monitor \"%s\" not found!\n", argv[0], argv[1]);
         return HERBST_INVALID_ARGUMENT;
     }
-    auto rect = parse_rectangle(argv[2]);
+    auto rect = Rectangle::fromStr(argv[2]);
     if (rect.width < WINDOW_MIN_WIDTH || rect.height < WINDOW_MIN_HEIGHT) {
         g_string_append_printf(output,
             "%s: Rectangle is too small\n", argv[0]);
@@ -757,9 +756,9 @@ void ensure_monitors_are_available() {
         return;
     }
     // add monitor if necessary
-    Rectangle rect = Rectangle(0,0,
+    Rectangle rect = { 0, 0,
             DisplayWidth(g_display, DefaultScreen(g_display)),
-            DisplayHeight(g_display, DefaultScreen(g_display)));
+            DisplayHeight(g_display, DefaultScreen(g_display))};
     ensure_tags_are_available();
     // add monitor with first tag
     HSMonitor* m = add_monitor(rect, get_tag_by_index(0), NULL);
@@ -1163,16 +1162,13 @@ static bool geom_unique(XineramaScreenInfo *unique, size_t n, XineramaScreenInfo
 }
 
 // inspired by dwm's updategeom()
-bool detect_monitors_xinerama(Rectangle** ret_rects, size_t* ret_count) {
+bool detect_monitors_xinerama(RectangleVec &ret) {
     int i, j, n;
-    XineramaScreenInfo *info, *unique;
-    Rectangle *monitors;
-
     if (!XineramaIsActive(g_display)) {
         return false;
     }
-    info = XineramaQueryScreens(g_display, &n);
-    unique = g_new(XineramaScreenInfo, n);
+    XineramaScreenInfo *info = XineramaQueryScreens(g_display, &n);
+    XineramaScreenInfo *unique = g_new(XineramaScreenInfo, n);
     /* only consider unique geometries as separate screens */
     for (i = 0, j = 0; i < n; i++) {
         if (geom_unique(unique, j, &info[i]))
@@ -1183,51 +1179,38 @@ bool detect_monitors_xinerama(Rectangle** ret_rects, size_t* ret_count) {
     XFree(info);
     n = j;
 
-    monitors = g_new(Rectangle, n);
+    RectangleVec monitors(n);
     for (i = 0; i < n; i++) {
         monitors[i].x = unique[i].x_org;
         monitors[i].y = unique[i].y_org;
         monitors[i].width = unique[i].width;
         monitors[i].height = unique[i].height;
     }
-    *ret_count = n;
-    *ret_rects = monitors;
+    ret.swap(monitors);
     g_free(unique);
     return true;
 }
 #else  /* XINERAMA */
 
-bool detect_monitors_xinerama(Rectangle** ret_rects, size_t* ret_count) {
+bool detect_monitors_xinerama(RectangleVec &dest) {
     return false;
 }
 
 #endif /* XINERAMA */
 
 // monitor detection that always works: one monitor across the entire screen
-bool detect_monitors_simple(Rectangle** ret_rects, size_t* ret_count) {
+bool detect_monitors_simple(RectangleVec &dest) {
     XWindowAttributes attributes;
     XGetWindowAttributes(g_display, g_root, &attributes);
-
-    *ret_count = 1;
-    *ret_rects = g_new0(Rectangle, 1);
-    (*ret_rects)->x = 0;
-    (*ret_rects)->y = 0;
-    (*ret_rects)->width = attributes.width;
-    (*ret_rects)->height = attributes.height;
+    dest = {{ 0, 0, attributes.width, attributes.height }};
     return true;
 }
 
-bool detect_monitors_debug_example(Rectangle** ret_rects, size_t* ret_count) {
-    *ret_count = 2;
-    *ret_rects = g_new0(Rectangle, 2);
-    (*ret_rects)[0].x = 0;
-    (*ret_rects)[0].y = 0;
-    (*ret_rects)[0].width = g_screen_width * 2 / 3;
-    (*ret_rects)[0].height = g_screen_height * 2 / 3;
-    (*ret_rects)[1].x = g_screen_width / 3;
-    (*ret_rects)[1].y = g_screen_height / 3;
-    (*ret_rects)[1].width = g_screen_width * 2 / 3;
-    (*ret_rects)[1].height = g_screen_height * 2 / 3;
+bool detect_monitors_debug_example(RectangleVec &dest) {
+    dest = {{ 0, 0,
+              g_screen_width * 2 / 3, g_screen_height * 2 / 3 },
+            { g_screen_width / 3, g_screen_height / 3,
+              g_screen_width * 2 / 3, g_screen_height * 2 / 3}};
     return true;
 }
 
@@ -1238,16 +1221,16 @@ int detect_monitors_command(int argc, const char **argv, GString* output) {
         detect_monitors_simple,
         detect_monitors_debug_example, // move up for debugging
     };
-    Rectangle* monitors = NULL;
+    RectangleVec monitors;
     size_t count = 0;
     // search for a working monitor detection
     // at least the simple detection must work
     for (int i = 0; i < LENGTH(detect); i++) {
-        if (detect[i](&monitors, &count)) {
+        if (detect[i](monitors)) {
             break;
         }
     }
-    assert(count && monitors);
+    assert(count && !monitors.empty());
     bool list_only = false;
     bool disjoin = true;
     //bool drop_small = true;
@@ -1274,9 +1257,9 @@ int detect_monitors_command(int argc, const char **argv, GString* output) {
     } else {
         // possibly disjoin them
         if (disjoin) {
-            RectList* rl = disjoin_rects(monitors, count);
+            RectList* rl = disjoin_rects(monitors);
             count = rectlist_length(rl);
-            monitors = g_renew(Rectangle, monitors, count);
+            monitors.resize(count);
             RectList* cur = rl;
             FOR (i,0,count) {
                 monitors[i] = cur->rect;
@@ -1284,13 +1267,12 @@ int detect_monitors_command(int argc, const char **argv, GString* output) {
             }
         }
         // apply it
-        ret = set_monitor_rects(monitors, count);
+        ret = set_monitor_rects(monitors);
         if (ret == HERBST_TAG_IN_USE && output != NULL) {
             g_string_append_printf(output,
                 "%s: There are not enough free tags\n", argv[0]);
         }
     }
-    g_free(monitors);
     return ret;
 }
 

src/decoration.cpp
@@ -41,30 +41,30 @@ void decorations_init() {
     // init default schemes
     // tiling //
     HSDecTriple tiling = {
-        { 2, getcolor("black"),     false },    // normal
-        { 2, getcolor("green"),     false },    // active
-        { 2, getcolor("orange"),    false },    // urgent
+        { 2, Color::fromStr("black"),     false },    // normal
+        { 2, Color::fromStr("green"),     false },    // active
+        { 2, Color::fromStr("orange"),    false },    // urgent
     };
     g_decorations[HSDecSchemeTiling] = tiling;
     // fullscreen //
     HSDecTriple fs = {
-        { 0, getcolor("black"),     false },    // normal
-        { 0, getcolor("black"),     false },    // active
-        { 0, getcolor("black"),     false },    // urgent
+        { 0, Color::fromStr("black"),     false },    // normal
+        { 0, Color::fromStr("black"),     false },    // active
+        { 0, Color::fromStr("black"),     false },    // urgent
     };
     g_decorations[HSDecSchemeFullscreen] = fs;
     // floating //
     HSDecTriple fl = {
-        { 1, getcolor("black"),     true  },    // normal
-        { 4, getcolor("green"),     true  },    // active
-        { 1, getcolor("orange"),    true  },    // urgent
+        { 1, Color::fromStr("black"),     true  },    // normal
+        { 4, Color::fromStr("green"),     true  },    // active
+        { 1, Color::fromStr("orange"),    true  },    // urgent
     };
     g_decorations[HSDecSchemeFloating] = fl;
     // minimal //
     HSDecTriple minimal = {
-        { 0, getcolor("black"),     true  },    // normal
-        { 0, getcolor("green"),     true  },    // active
-        { 0, getcolor("orange"),    true  },    // urgent
+        { 0, Color::fromStr("black"),     true  },    // normal
+        { 0, Color::fromStr("green"),     true  },    // active
+        { 0, Color::fromStr("orange"),    true  },    // urgent
     };
     g_decorations[HSDecSchemeMinimal] = minimal;
     init_dec_triple_object(g_decorations + HSDecSchemeTiling, "tiling");
@@ -350,21 +350,21 @@ HSClient* get_client_from_decoration(Window decwin) {
 }
 
 Rectangle outline_to_inner_rect(Rectangle rect, HSDecorationScheme s) {
-    return Rectangle(
+    return {
         rect.x + s.border_width + s.padding_left,
         rect.y + s.border_width + s.padding_top,
         rect.width  - 2* s.border_width - s.padding_left - s.padding_right,
         rect.height - 2* s.border_width - s.padding_top - s.padding_bottom
-    );
+    };
 }
 
 Rectangle inner_rect_to_outline(Rectangle rect, HSDecorationScheme s) {
-    return Rectangle(
+    return {
         rect.x - s.border_width - s.padding_left,
         rect.y - s.border_width - s.padding_top,
         rect.width  + 2* s.border_width + s.padding_left + s.padding_right,
         rect.height + 2* s.border_width + s.padding_top + s.padding_bottom
-    );
+    };
 }
 
 void decoration_resize_inner(HSClient* client, Rectangle inner,

src/decoration.h
@@ -22,18 +22,18 @@ struct HSClient;
 
 typedef struct {
     int     border_width;
-    HSColor border_color;
+    Color border_color;
     bool    tight_decoration; // if set, there is no space between the
                               // decoration and the window content
-    HSColor inner_color;
+    Color inner_color;
     int     inner_width;
-    HSColor outer_color;
+    Color outer_color;
     int     outer_width;
     int     padding_top;    // additional window border
     int     padding_right;  // additional window border
     int     padding_bottom; // additional window border
     int     padding_left;   // additional window border
-    HSColor background_color; // color behind client contents
+    Color background_color; // color behind client contents
 } HSDecorationScheme;
 
 typedef struct {

src/layout.cpp
@@ -91,16 +91,16 @@ static void fetch_frame_colors() {
     g_focus_crosses_monitor_boundaries = &(settings_find("focus_crosses_monitor_boundaries")->value.i);
     *g_default_frame_layout = CLAMP(*g_default_frame_layout, 0, LAYOUT_COUNT - 1);
     char* str = settings_find_string("frame_border_normal_color");
-    g_frame_border_normal_color = getcolor(str);
+    g_frame_border_normal_color = Color::fromStr(str);
     str = settings_find_string("frame_border_active_color");
-    g_frame_border_active_color = getcolor(str);
+    g_frame_border_active_color = Color::fromStr(str);
     str = settings_find_string("frame_border_inner_color");
-    g_frame_border_inner_color = getcolor(str);
+    g_frame_border_inner_color = Color::fromStr(str);
     // background color
     str = settings_find_string("frame_bg_normal_color");
-    g_frame_bg_normal_color = getcolor(str);
+    g_frame_bg_normal_color = Color::fromStr(str);
     str = settings_find_string("frame_bg_active_color");
-    g_frame_bg_active_color = getcolor(str);
+    g_frame_bg_active_color = Color::fromStr(str);
     g_frame_active_opacity = CLAMP(settings_find("frame_active_opacity")->value.i, 0, 100);
     g_frame_normal_opacity = CLAMP(settings_find("frame_normal_opacity")->value.i, 0, 100);
 
@@ -138,7 +138,7 @@ HSFrame* frame_create_empty(HSFrame* parent, HSTag* parenttag) {
     frame->tag = parent ? parent->tag : parenttag;
     // set window attributes
     XSetWindowAttributes at;
-    at.background_pixel  = getcolor("red");
+    at.background_pixel  = Color::fromStr("red");
     at.background_pixmap = ParentRelative;
     at.override_redirect = True;
     at.bit_gravity       = StaticGravity;

src/monitor.h
@@ -74,7 +74,7 @@ int list_monitors(int argc, char** argv, GString* output);
 int list_padding(int argc, char** argv, GString* output);
 int set_monitor_rects_command(int argc, char** argv, GString* output);
 int disjoin_rects_command(int argc, char** argv, GString* output);
-int set_monitor_rects(Rectangle* templates, size_t count);
+int set_monitor_rects(const RectangleVec &templates);
 int move_monitor_command(int argc, char** argv, GString* output);
 int rename_monitor_command(int argc, char** argv, GString* output);
 int monitor_rect_command(int argc, char** argv, GString* output);
@@ -107,9 +107,9 @@ struct HSStack* get_monitor_stack();
 
 void monitor_update_focus_objects();
 
-typedef bool (*MonitorDetection)(Rectangle**, size_t*);
-bool detect_monitors_xinerama(Rectangle** ret_rects, size_t* ret_count);
-bool detect_monitors_simple(Rectangle** ret_rects, size_t* ret_count);
+typedef bool (*MonitorDetection)(RectangleVec &);
+bool detect_monitors_xinerama(RectangleVec &dest);
+bool detect_monitors_simple(RectangleVec &dest);
 int detect_monitors_command(int argc, const char **argv, GString* output);
 
 int shift_to_monitor(int argc, char** argv, GString* output);

src/object.cpp
@@ -581,7 +581,7 @@ int hsattribute_assign(HSAttribute* attr, const char* new_value_str, GString* ou
             break;
 
         case HSATTR_TYPE_COLOR:
-            error = !getcolor_error(new_value_str, &new_value.color);
+            error = !Color::convert(new_value_str, new_value.color);
             if (error) {
                 g_string_append_printf(output,
                     "\"%s\" is not a valid color.", new_value_str);
@@ -762,7 +762,7 @@ int compare_command(int argc, char* argv[], GString* output) {
         return HERBST_INVALID_ARGUMENT;
     } else if (attr->type == HSATTR_TYPE_COLOR) {
         auto l = *attr->value.color;
-        auto r = getcolor(rvalue);
+        auto r = Color::fromStr(rvalue);
         if (!strcmp("=", op)) return !(l == r);
         if (!strcmp("!=", op)) return !(l != r);
         g_string_append_printf(output, "Invalid color operator \"%s\"", op);
@@ -901,7 +901,7 @@ HSAttribute* hsattribute_create(HSObject* obj, const char* name, char* type_str,
             attr->value.str = &attr->user_data->str;
             break;
         case HSATTR_TYPE_COLOR:
-            attr->user_data->color = getcolor("#000000");
+            attr->user_data->color = Color::fromStr("#000000");
             attr->unparsed_value = g_string_new("#000000");
             attr->value.color = &attr->user_data->color;
         default:

src/object.h
@@ -34,14 +34,14 @@ typedef union HSAttributePointer {
     int*        i;
     unsigned int* u;
     GString**   str;
-    HSColor*    color;
+    Color*    color;
     HSAttributeCustom custom;
     HSAttributeCustomInt custom_int;
     HSAttributePointer(bool* b) : b(b) { };
     HSAttributePointer(int* x) : i(x) { };
     HSAttributePointer(unsigned int* x) : u(x) { };
     HSAttributePointer(GString** x) : str(x) { };
-    HSAttributePointer(HSColor* x) : color(x) { };
+    HSAttributePointer(Color* x) : color(x) { };
     HSAttributePointer(HSAttributeCustom x) : custom(x) { };
     HSAttributePointer(HSAttributeCustomInt x) : custom_int(x) { };
 } HSAttributePointer;
@@ -51,7 +51,7 @@ typedef union HSAttributeValue {
     int         i;
     unsigned int u;
     GString*    str;
-    HSColor     color;
+    Color     color;
 } HSAttributeValue;
 
 typedef GString* (*HSAttrCallback)(HSAttribute* attr);
@@ -114,7 +114,7 @@ public:
     HSAttributeSimpleConstructor(HSATTR_TYPE_BOOL, bool) {};
     HSAttributeSimpleConstructor(HSATTR_TYPE_INT, int) {};
     HSAttributeSimpleConstructor(HSATTR_TYPE_UINT, unsigned int) {};
-    HSAttributeSimpleConstructor(HSATTR_TYPE_COLOR, HSColor) {
+    HSAttributeSimpleConstructor(HSATTR_TYPE_COLOR, Color) {
         unparsed_value = g_string_new("");
     };
     HSAttributeSimpleConstructor(HSATTR_TYPE_STRING, GString*) {};

src/utils.cpp
@@ -56,22 +56,15 @@ void die(const char *errstr, ...) {
 
 // get X11 color from color string
 // from dwm.c
-unsigned long getcolor(const char *colstr) {
-    HSColor ret_color;
-    if (!getcolor_error(colstr, &ret_color)) {
-        ret_color = 0;
-    }
-    return ret_color;
-}
-
-bool getcolor_error(const char *colstr, HSColor* ret_color) {
+bool Color::convert(const char *source, Color& dest) {
     Colormap cmap = DefaultColormap(g_display, g_screen);
     XColor color;
-    if(!XAllocNamedColor(g_display, cmap, colstr, &color, &color)) {
-        g_warning("error, cannot allocate color '%s'\n", colstr);
+    if(!XAllocNamedColor(g_display, cmap, source, &color, &color)) {
+        g_warning("error, cannot allocate color '%s'\n", source);
         return false;
     }
-    *ret_color = color.pixel;
+    dest = color.pixel;
+    //dest.name_ = source;
     return true;
 }
 
@@ -188,16 +181,17 @@ void argv_free(int argc, char** argv) {
 }
 
 
-Rectangle parse_rectangle(char* string) {
-    Rectangle rect;
-    int x,y;
+Rectangle Rectangle::fromStr(const char* source) {
+    int x, y;
     unsigned int w, h;
-    int flags = XParseGeometry(string, &x, &y, &w, &h);
-    rect.x = (XValue & flags) ? (short int)x : 0;
-    rect.y = (YValue & flags) ? (short int)y : 0;
-    rect.width = (WidthValue & flags) ? (unsigned short int)w : 0;
-    rect.height = (HeightValue & flags) ? (unsigned short int)h : 0;
-    return rect;
+    int flags = XParseGeometry(source, &x, &y, &w, &h);
+
+    return {
+        (XValue & flags) ? (short int)x : 0,
+        (YValue & flags) ? (short int)y : 0,
+        (WidthValue & flags) ? (unsigned short int)w : 0,
+        (HeightValue & flags) ? (unsigned short int)h : 0
+    };
 }
 
 const char* strlasttoken(const char* str, const char* delim) {

commit 959468d14a232eb1edfebd92b6f7f7fa15eaa45c

Fix regression introduced in 1c84e669cd8f5a1cf3f7b4ad84a73b9685226d1b
Remove unusable variable count

src/monitor.cpp
@@ -1228,7 +1228,6 @@ int detect_monitors_command(int argc, const char **argv, GString* output) {
         detect_monitors_debug_example, // move up for debugging
     };
     RectangleVec monitors;
-    size_t count = 0;
     // search for a working monitor detection
     // at least the simple detection must work
     for (int i = 0; i < LENGTH(detect); i++) {
@@ -1236,7 +1235,7 @@ int detect_monitors_command(int argc, const char **argv, GString* output) {
             break;
         }
     }
-    assert(count && !monitors.empty());
+    assert(!monitors.empty());
     bool list_only = false;
     bool disjoin = true;
     //bool drop_small = true;
@@ -1255,19 +1254,18 @@ int detect_monitors_command(int argc, const char **argv, GString* output) {
 
     int ret = 0;
     if (list_only) {
-        FOR (i,0,count) {
+        for (auto m : monitors) {
             g_string_append_printf(output, "%dx%d%+d%+d\n",
-                monitors[i].width, monitors[i].height,
-                monitors[i].x, monitors[i].y);
+                m.width, m.height,
+                m.x, m.y);
         }
     } else {
         // possibly disjoin them
         if (disjoin) {
             RectList* rl = disjoin_rects(monitors);
-            count = rectlist_length(rl);
-            monitors.resize(count);
+            monitors.resize(rectlist_length(rl));
             RectList* cur = rl;
-            FOR (i,0,count) {
+            FOR (i,0,monitors.size()) {
                 monitors[i] = cur->rect;
                 cur = cur->next;
             }