37aef322d975130d3ef825a544765c60a8707c2d
Thorsten Wißmann <edu@thorsten-wissmann.de>
2015-09-01 16:40:50 +0200

    Let wselect.sh respect swap_monitors_to_get_tag

--- scripts/wselect.sh
+++ scripts/wselect.sh
@@ -16,7 +16,24 @@ case "$1" in
         action() { hc bring "$@" ; }
         ;;
 
-    select|*)
+    select_here|*)
+        # first focus the tag of the selected window and then select the window
+        # this enforces that the setting swap_monitors_to_get_tag is respected:
+        # if set, the tag is brought to the focused monitor and the window gets focused.
+        # if unset, the focused jumps to the desired window and its position on
+        # the screen(s) remains the same.
+        name=Select:
+        action() {
+            local winid=$(sed 's,0x[0]*,0x,' <<< "$*")
+            local tag=$(hc attr clients."$winid".tag)
+            hc lock
+            hc use "$tag"
+            hc jumpto "$*"
+            hc unlock
+        }
+        ;;
+
+    select)
         # switch to the selected window and focus it
         action() { hc jumpto "$@" ; }
         name=Select:
