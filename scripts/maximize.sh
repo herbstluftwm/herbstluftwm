3fdae321c731eaeaa69475d7ea1fd3eab99c544a
Thorsten Wißmann <edu@thorsten-wissmann.de>
2015-10-12 14:23:14 +0200

    Improve maximize.sh
    Add comments and don't "unmaximize" if the user did that already
    manually.

--- scripts/maximize.sh
+++ scripts/maximize.sh
@@ -17,16 +17,25 @@
 mode=${1:-max} # just some valid layout algorithm name
 
 # FIXME: for some unknown reason, remove_attr always fails
-#        fix that and remove the "try" afterwards
+#        fix that in the hlwm core and remove the "try" afterwards
 layout=$(herbstclient dump)
 cmd=(
+# remmember which client is focused
 substitute FOCUS clients.focus.winid chain
 . lock
-. or : and , silent substitute STR tags.focus.my_unmaximized_layout load STR
+. or : and # if there is more than one frame, then don't restore, but maximize again!
+           , compare tags.focus.frame_count = 1
+           # if we have such a stored layout, then restore it, else maximize
+           , silent substitute STR tags.focus.my_unmaximized_layout load STR
+           # remove the stored layout
            , try remove_attr tags.focus.my_unmaximized_layout
      : chain , new_attr string tags.focus.my_unmaximized_layout
+             # save the current layout in the attribute
              , set_attr tags.focus.my_unmaximized_layout "$layout"
+             # force all windows into a single frame in max layout
              , load "(clients $mode:0 )"
+# both load commands accidentally change the window focus, so restore the
+# window focus from before the "load" command
 . jumpto FOCUS
 . unlock
 )
