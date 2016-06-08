22767f74905af0450eba23c79911475d58c3690b
Manuel <manuel.coenen@gmail.com>
2016-06-08 14:05:39 +0200

    Fix issue with commands having more completions
    When trying to complete "herbstclient use <tab>" it would not provide correct results as the space before tab was not considered and it only provided completions for command starting with "use". Additionally it printed the string "herbstclient" which was due to a missing --quiet parameter when using fish builtin string.

--- share/herbstclient.fish
+++ share/herbstclient.fish
@@ -3,44 +3,35 @@
 function _get_herbstluftwm_completion
     set -l tokens (commandline -op)
 
-    # Filter herbstclient related tokens
-    set -l index 1
-    for token in $tokens
-        if string match -- "$token" "herbstclient"
-            or string match -- "-" (string sub -l 1 -- $token)
+    # Delete "herbstclient"
+    set -e tokens[1]
+
+    # Filter herbstclient options
+    set -l index (count $tokens)
+    for token in $tokens[-1..1]
+        if string match --quiet -- "-" (string sub -l 1 -- $token)
             set -e tokens[$index]
         end
-        set index (math "$index + 1")
+        set index (math "$index - 1")
     end
 
-    # If no tokens remain return everything
-    if test (count $tokens) -eq 0
-        herbstclient -q complete 0
-        return
-    end
+    set -l position (count $tokens)
 
-    # Position is zero-based in hlwm
-    set -l position (math (count $tokens) - 1)
-    set -l completions (herbstclient -q complete $position $tokens)
-
-    # If the last parameter equals the only result increase the position to get the next token
-    if test (count $completions) -eq 1
-        and string match -- "$completions" "$tokens[-1]"
-        set position (math $position + 1)
-        herbstclient -q complete $position $tokens
-    else
-        string replace --all -- ' ' \n "$completions"
+    # If we have tokens but and cursor sits on a word (not space) decrease position due to 0-based in hlwm and 1-based in fish
+    if test $position -gt 0 -a -n (commandline -ct)
+        set position (math "$position - 1")
     end
+    herbstclient -q complete $position $tokens
 end
 
 complete -xc herbstclient -d "" -a '(_get_herbstluftwm_completion)'
 
-complete -c herbstclient -f -s n -l no-newline -d 'Do not print a newline if output does not end with a newline.'
-complete -c herbstclient -f -s 0 -l print0 -d 'Use the null character as delimiter between the output of hooks.'
-complete -c herbstclient -f -s l -l last-arg -d 'When using -i or -w, only print the last argument of the hook.'
-complete -c herbstclient -f -s i -l idle -d 'Wait for hooks instead of executing commands.'
-complete -c herbstclient -f -s w -l wait -d 'Same as --idle but exit after first --count hooks.'
-complete -c herbstclient -f -s c -l count -r -d 'Let --wait exit after COUNT hooks were received and printed.'
-complete -c herbstclient -f -s q -l quiet -d 'Do not print error messages if herbstclient cannot connect to the running herbstluftwm instance.'
-complete -c herbstclient -f -s v -l version -d 'Print the herbstclient version.'
-complete -c herbstclient -f -s h -l help -d 'Print the herbstclient usage with its command line options.'
+complete -fc herbstclient -s n -l no-newline -d 'Do not print a newline if output does not end with a newline.'
+complete -fc herbstclient -s 0 -l print0 -d 'Use the null character as delimiter between the output of hooks.'
+complete -fc herbstclient -s l -l last-arg -d 'When using -i or -w, only print the last argument of the hook.'
+complete -fc herbstclient -s i -l idle -d 'Wait for hooks instead of executing commands.'
+complete -fc herbstclient -s w -l wait -d 'Same as --idle but exit after first --count hooks.'
+complete -fc herbstclient -s c -l count -r -d 'Let --wait exit after COUNT hooks were received and printed.'
+complete -fc herbstclient -s q -l quiet -d 'Do not print error messages if herbstclient cannot connect to the running herbstluftwm instance.'
+complete -fc herbstclient -s v -l version -d 'Print the herbstclient version.'
+complete -fc herbstclient -s h -l help -d 'Print the herbstclient usage with its command line options.'
