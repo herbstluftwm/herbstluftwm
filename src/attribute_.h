7247a636a782cf1d8b2f2371f2e6cd36e90f8408
Thorsten Wißmann <edu@thorsten-wissmann.de>
2018-07-31 11:46:55 +0200

    Only a static parse function for each type
    Extract a static parse function for each implementation of
    Attribute_<T>. This removes redundancy and makes the parse function
    usable outside of attribute assignment.

--- src/attribute_.h
+++ src/attribute_.h
@@ -5,6 +5,7 @@
 #include "object.h"
 #include "x11-types.h" // for hl::Color
 #include <functional>
+#include <stdexcept>
 
 
 template<typename T>
@@ -47,27 +48,43 @@ public:
     // this assigns and checks the validity of the data
     // in case of further constraints.
     // (i.e. a tag name should not clash with any other tag name)
-    std::string assignByUser(const T &payload) {
-        T old_payload = payload_;
-        payload_ = payload;
-        std::string error_message = this->writeable() ? (m_onChange)() : "";
-        if (error_message == "") {
-            // no error -> keep value
-            notifyHooks();
-            return {};
-        } else {
-            // error -> restore value
-            payload_ = old_payload;
-            return error_message;
-        }
-    }
+
+public:
+
     bool operator==(const T &payload) {
         return payload_ == payload;
     }
     bool operator!=(const T &payload) {
         return payload_ != payload;
     }
-    std::string change(const std::string &payload);
+
+    /** parse a text into the right type,
+     * possibly raising a std::invalid_argument exception. The string can be
+     * a specification in relative to 'reference', e.g. "toggle" for booleans.
+     */
+    static T parse(const std::string& source, T const* reference);
+
+    std::string change(const std::string &payload_str) {
+        try {
+            T new_payload = parse(payload_str, &payload_);
+            T old_payload = payload_;
+            payload_ = new_payload;
+            std::string error_message = this->writeable() ? (m_onChange)() : "";
+            if (error_message == "") {
+                // no error -> keep value
+                notifyHooks();
+                return {};
+            } else {
+                // error -> restore value
+                payload_ = old_payload;
+                return error_message;
+            }
+        } catch (std::invalid_argument const& e) {
+            return std::string("invalid argument: ") + e.what();
+        } catch (std::out_of_range const& e) {
+            return std::string("out of range: ") + e.what();
+        }
+    }
 
     const T& operator*() const {
         return payload_;
@@ -95,12 +112,8 @@ template<>
 inline Type Attribute_<int>::type() { return Type::ATTRIBUTE_INT; }
 
 template<>
-inline std::string Attribute_<int>::change(const std::string &payload) {
-    try {
-        return assignByUser(std::stoi(payload));
-    } catch (std::exception const& e) {
-        return "not a valid integer";
-    }
+inline int Attribute_<int>::parse(const std::string &payload, int const*) {
+    return std::stoi(payload);
 }
 
 /** Unsigned **/
@@ -109,8 +122,8 @@ template<>
 inline Type Attribute_<unsigned long>::type() { return Type::ATTRIBUTE_ULONG; }
 
 template<>
-inline std::string Attribute_<unsigned long>::change(const std::string &payload) {
-    return assignByUser(std::stoul(payload));
+inline unsigned long Attribute_<unsigned long>::parse(const std::string &payload, unsigned long const*) {
+    return std::stoul(payload);
 }
 
 /** Boolean **/
@@ -124,17 +137,17 @@ inline std::string Attribute_<bool>::str() {
 }
 
 template<>
-inline std::string Attribute_<bool>::change(const std::string &payload) {
-    bool new_data = payload_;
+inline bool Attribute_<bool>::parse(const std::string &payload, bool const* ref) {
     if (payload == "off" || payload == "false")
-        new_data = false;
+        return false;
     else if (payload == "on" || payload == "true")
-        new_data = true;
-    else if (payload == "toggle")
-        new_data = !new_data;
-    else
-        return "only on/off/true/false/toggle are valid booleans";
-    return assignByUser(new_data);
+        return true;
+    else if (payload == "toggle" && ref != NULL)
+        return ! ref;
+    else throw std::invalid_argument(
+            (ref != NULL)
+            ? "only on/off/true/false/toggle are valid booleans"
+            : "only on/off/true/false are valid booleans");
 }
 
 /** STRING **/
@@ -146,8 +159,8 @@ template<>
 inline std::string Attribute_<std::string>::str() { return payload_; }
 
 template<>
-inline std::string Attribute_<std::string>::change(const std::string &payload) {
-    return assignByUser(payload);
+inline std::string Attribute_<std::string>::parse(const std::string &payload, std::string const* ref) {
+    return payload;
 }
 
 /** COLOR **/
@@ -159,11 +172,11 @@ template<>
 inline std::string Attribute_<Color>::str() { return payload_.str(); }
 
 template<>
-inline std::string Attribute_<Color>::change(const std::string &payload) {
+inline Color Attribute_<Color>::parse(const std::string &payload, Color const* ref) {
     Color new_color;
     std::string msg = Color::fromStr(payload, new_color);
-    if (msg != "") return msg;
-    return assignByUser(new_color);
+    if (msg != "") throw std::invalid_argument(msg);
+    return new_color;
 }
 
 template<typename T>
