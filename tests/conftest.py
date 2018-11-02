7fafb4f3124b63f9bbb2abf816392d5abcf768c2
Thorsten Wißmann <edu@thorsten-wissmann.de>
2018-11-02 22:05:33 +0100

    tests: New bridge utility for creating clients

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,13 +14,20 @@ class HlwmBridge:
 
     HC_PATH = os.path.join(GIT_ROOT, 'herbstclient')
 
+    def __init__(self,display):
+        self.next_client_id = 0;
+        self.env = {
+            'DISPLAY': display,
+        }
+
     def callstr(self, args, check=True):
         return self.call(*(args.split(' ')), check=check)
     def call(self, *args, check=True):
         assert args
         str_args = [ str(i) for i in args]
-        proc = subprocess.run([self.HC_PATH, '-n'] + str_args, check=check,
+        proc = subprocess.run([self.HC_PATH, '-n'] + str_args,
                               stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                              env=self.env,
                               universal_newlines=True)
         print(list(args))
         print(proc.stdout)
@@ -35,9 +42,29 @@ class HlwmBridge:
     def get_attr(self, attribute_path, check=True):
         return self.call('get_attr', attribute_path).stdout
 
+    def create_client(self):
+        self.next_client_id += 1
+        wmclass = 'client_{}'.format(self.next_client_id)
+        command = ['xterm', '-hold', '-class', wmclass, '-e', 'true']
+        # enforce a hook when the window appears
+        self.call('rule', 'once', 'class='+wmclass, 'hook=here_is_'+wmclass)
+        # run process that wait for the hook
+        hc_wait = subprocess.Popen([self.HC_PATH, '--wait', 'rule', 'here_is_'+wmclass],
+                    env=self.env,
+                    stdout=subprocess.PIPE)
+        # start the process...
+        proc = subprocess.Popen(command,env=self.env)
+        # once the window appears, the hook is fired, and the --wait exits:
+        hc_wait.wait(2)
+        winid = hc_wait.stdout.read().decode().rstrip('\n').split('\t')[-1]
+        return (proc,winid)
+
+
 @pytest.fixture
 def hlwm():
-    return HlwmBridge()
+    display = os.environ['DISPLAY']
+    #display = ':13'
+    return HlwmBridge(display)
 
 
 
@@ -47,6 +74,7 @@ def hlwm_process(tmpdir):
         'DISPLAY': os.environ['DISPLAY'],
         'XDG_CONFIG_HOME': str(tmpdir),
     }
+    #env['DISPLAY'] = ':13'
     autostart = tmpdir / 'herbstluftwm' / 'autostart'
     autostart.ensure()
     autostart.write(textwrap.dedent("""
@@ -63,4 +91,5 @@ def hlwm_process(tmpdir):
 
     yield
     proc.terminate()
+    #proc.wait()
     assert proc.wait(2) == 0
