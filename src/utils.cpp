herbstluftwm/herbstluftwm

commit ce98777736698bed7cef35db81680ee4bb36d87f

Fix some more sign warnings

src/utils.cpp
@@ -367,9 +367,9 @@ void g_queue_remove_element(GQueue* queue, GList* elem) {
 }
 
 int array_find(const void* buf, size_t elems, size_t size, const void* needle) {
-    for (int i = 0; i < elems; i++) {
+    for (size_t i = 0; i < elems; i++) {
         if (0 == memcmp((const char*)buf + (size * i), needle, size)) {
-            return i;
+            return (int)i;
         }
     }
     return -1;
@@ -378,7 +378,7 @@ int array_find(const void* buf, size_t elems, size_t size, const void* needle) {
 void array_reverse(void* void_buf, size_t elems, size_t size) {
     char* buf = (char*)void_buf;
     char* tmp = new char[size];
-    for (int i = 0, j = elems - 1; i < j; i++, j--) {
+    for (size_t i = 0, j = elems - 1; i < j; i++, j--) {
         memcpy(tmp, buf + size * i, size);
         memcpy(buf + size * i, buf + size * j, size);
         memcpy(buf + size * j, tmp, size);
@@ -453,7 +453,7 @@ void set_window_double_border(Display *dpy, Window win, int ibw,
     int width = wa.width;
     int height = wa.height;
 
-    unsigned int depth = wa.depth;
+    auto depth = (unsigned)wa.depth;
 
     int full_width = width + 2 * bw;
     int full_height = height + 2 * bw;
@@ -608,7 +608,7 @@ char* posix_sh_escape(const char* source) {
             }
         }
     }
-    size_t source_len = i;
+    auto source_len = (size_t)i;
     // special chars:
     if (source[0] == '~') {
         count++;

src/layout.cpp
@@ -399,21 +399,21 @@ char* load_frame_tree(shared_ptr<HSFrame> frame, char* description, Output outpu
 */
 
 int find_layout_by_name(char* name) {
-    for (int i = 0; i < LENGTH(g_layout_names); i++) {
+    for (size_t i = 0; i < LENGTH(g_layout_names); i++) {
         if (!g_layout_names[i]) {
             break;
         }
         if (!strcmp(name, g_layout_names[i])) {
-            return i;
+            return (int)i;
         }
     }
     return -1;
 }
 
 int find_align_by_name(char* name) {
-    for (int i = 0; i < LENGTH(g_align_names); i++) {
+    for (size_t i = 0; i < LENGTH(g_align_names); i++) {
         if (!strcmp(name, g_align_names[i])) {
-            return i;
+            return (int)i;
         }
     }
     return -1;
@@ -533,6 +533,7 @@ int frame_current_cycle_client_layout(int argc, char** argv, Output output) {
         const char* curname = g_layout_names[cur_frame->getLayout()];
         char** pcurrent = (char**)table_find(argv, sizeof(*argv), argc, 0,
                                      memberequals_string, curname);
+        // signed for delta calculations
         int idx = pcurrent ? (INDEX_OF(argv, pcurrent) + delta) : 0;
         idx %= argc;
         idx += argc;

src/main.cpp
@@ -925,7 +925,7 @@ int main(int argc, char* argv[]) {
 
 
     // initialize subsystems
-    for (int i = 0; i < LENGTH(g_modules); i++) {
+    for (unsigned i = 0; i < LENGTH(g_modules); i++) {
         g_modules[i].init();
     }
 

commit 393c6dc2a6138a9e99310209cb58fec7df910fad

Fix regression introduced by ce98777

src/utils.cpp
@@ -378,7 +378,7 @@ int array_find(const void* buf, size_t elems, size_t size, const void* needle) {
 void array_reverse(void* void_buf, size_t elems, size_t size) {
     char* buf = (char*)void_buf;
     char* tmp = new char[size];
-    for (size_t i = 0, j = elems - 1; i < j; i++, j--) {
+    for (int i = 0, j = elems - 1; i < j; i++, j--) {
         memcpy(tmp, buf + size * i, size);
         memcpy(buf + size * i, buf + size * j, size);
         memcpy(buf + size * j, tmp, size);