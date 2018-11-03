97e5968a373c5cb289f32a6b932f56e7fae01bd3
Thorsten Wißmann <edu@thorsten-wissmann.de>
2018-11-03 18:39:46 +0100

    floating command: only parse bool if needed

--- src/tag.cpp
+++ src/tag.cpp
@@ -172,12 +172,13 @@ int tag_set_floating_command(int argc, char** argv, Output output) {
         }
     }
 
-    bool new_value = string_to_bool(action, tag->floating);
-
     if (!strcmp(action, "status")) {
         // just print status
         output << (tag->floating ? "on" : "off");
     } else {
+        // after deleting this, delete include utils.h line
+        bool new_value = string_to_bool(action, tag->floating);
+
         // assign new value and rearrange if needed
         tag->floating = new_value;
 
