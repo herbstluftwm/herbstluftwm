2337c27a7dbd5159dfd97aad51ba4d9e912cb2bf
Daniel Danner <daniel@danner.de>
2018-11-18 17:29:24 +0100

    Fix three more occasions

--- src/main.cpp
+++ src/main.cpp
@@ -628,7 +628,7 @@ static void parse_arguments(int argc, char** argv, Globals& g) {
         {}
     };
     // parse options
-    while (1) {
+    while (true) {
         int option_index = 0;
         int c = getopt_long(argc, argv, "+c:vl", long_options, &option_index);
         if (c == -1) break;
