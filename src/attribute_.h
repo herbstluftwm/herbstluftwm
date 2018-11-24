e6b01546ed457636b22a5f6813a38da2ac806c28
Johannes Jordan <git@lanrules.de>
2018-11-24 15:30:03 +0100

    Move conversion logic to types.h

--- src/attribute_.h
+++ src/attribute_.h
@@ -4,8 +4,6 @@
 #include "attribute.h"
 #include "object.h"
 #include "signal.h"
-#include "x11-types.h" // for hl::Color
-#include <set>
 #include <functional>
 #include <stdexcept>
 
@@ -32,12 +30,13 @@ public:
         writeable_ = true;
     }
 
-    // delegate type(), str() to a static methods in specializations,
-    // so they can also be used by DynAttribute_<T>
+    // delegate type() to a static methods in specializations,
+    // so it can also be used by DynAttribute_<T>
     Type type() override { return Attribute_<T>::staticType(); }
-    std::string str() override { return Attribute_<T>::str(payload_); }
     static Type staticType();
-    static std::string str(T payload) { return std::to_string(payload); }
+
+    // wrap Converter::str() for convenience
+    std::string str() override { return Converter<T>::str(payload_); }
 
     Signal_<T>& changed() override { return changed_; }
 
@@ -58,16 +57,10 @@ public:
         return payload_ != payload;
     }
 
-    /** parse a text into the right type,
-     * possibly raising a std::invalid_argument exception. The string can be
-     * a specification in relative to 'previous', e.g. "toggle" for booleans.
-     */
-    static T parse(const std::string& source, T const* previous);
-
     std::string change(const std::string &payload_str) override {
         if (!writeable()) return "attribute is read-only";
         try {
-            T new_payload = parse(payload_str, &payload_); // throws
+            T new_payload = Converter<T>::parse(payload_str, &payload_); // throws
 
             // validate, if needed
             if (validator_) {
@@ -109,80 +102,17 @@ protected:
     T payload_;
 };
 
-/** Integer **/
-
+/** Type mappings **/
 template<>
 inline Type Attribute_<int>::staticType() { return Type::ATTRIBUTE_INT; }
-
-template<>
-inline int Attribute_<int>::parse(const std::string &payload, int const*) {
-    return std::stoi(payload);
-}
-
-/** Unsigned **/
-
 template<>
 inline Type Attribute_<unsigned long>::staticType() { return Type::ATTRIBUTE_ULONG; }
-
-template<>
-inline unsigned long Attribute_<unsigned long>::parse(const std::string &payload, unsigned long const*) {
-    return std::stoul(payload);
-}
-
-/** Boolean **/
-
 template<>
 inline Type Attribute_<bool>::staticType() { return Type::ATTRIBUTE_BOOL; }
-
-template<>
-inline std::string Attribute_<bool>::str(bool payload) {
-    return { payload ? "true" : "false" };
-}
-
-template<>
-inline bool Attribute_<bool>::parse(const std::string &payload, bool const* previous) {
-    std::set<std::string> t = {"true", "on", "1"};
-    std::set<std::string> f = {"false", "off", "0"};
-    if (f.find(payload) != f.end())
-        return false;
-    else if (t.find(payload) != t.end())
-        return true;
-    else if (payload == "toggle" && previous)
-        return !*previous;
-    else throw std::invalid_argument(
-            previous
-            ? "only on/off/true/false/toggle are valid booleans"
-            : "only on/off/true/false are valid booleans");
-}
-
-/** STRING **/
-
 template<>
 inline Type Attribute_<std::string>::staticType() { return Type::ATTRIBUTE_STRING; }
-
-template<>
-inline std::string Attribute_<std::string>::str(std::string payload) { return payload; }
-
-template<>
-inline std::string Attribute_<std::string>::parse(const std::string &payload, std::string const*) {
-    return payload;
-}
-
-/** COLOR **/
-
 template<>
 inline Type Attribute_<Color>::staticType() { return Type::ATTRIBUTE_COLOR; }
-
-template<>
-inline std::string Attribute_<Color>::str(Color payload) { return payload.str(); }
-
-template<>
-inline Color Attribute_<Color>::parse(const std::string &payload, Color const*) {
-    Color new_color;
-    std::string msg = Color::fromStr(payload, new_color);
-    if (msg != "") throw std::invalid_argument(msg);
-    return new_color;
-}
 
 template<typename T>
 class DynAttribute_ : public Attribute {
@@ -213,18 +143,14 @@ public:
                     "No change signalling on dynamic attributes.");
     }
 
-    static T parse(const std::string& source) {
-        return Attribute_<T>::parse(source, {});
-    }
-
     std::string str() override {
-        return Attribute_<T>::str(getter_());
+        return Converter<T>::str(getter_());
     }
 
     std::string change(const std::string &payload_str) override {
         if (!writeable()) return "attribute is read-only";
         try {
-            T new_payload = parse(payload_str); // throws
+            T new_payload = Converter<T>::parse(payload_str, nullptr); // throws
             return setter_(new_payload);
         } catch (std::invalid_argument const& e) {
             return std::string("invalid argument: ") + e.what();
--- src/types.h
+++ src/types.h
@@ -2,6 +2,8 @@
 #define HERBSTLUFT_TYPES_H
 
 #include "arglist.h"
+#include "x11-types.h" // for hl::Color
+#include <set>
 
 /* A path in the object tree */
 using Path = ArgList;
@@ -9,5 +11,69 @@ using Path = ArgList;
 /* Types for I/O with the user */
 using Input = ArgList;
 using Output = std::ostream&;
+
+/* Primitive types that can be converted from/to user input/output */
+template<typename T>
+struct Converter {
+    /** Parse a text into the right type
+     * Throws std::invalid_argument or std::out_of_range
+     * 'Source' may be given relative to 'relativeTo', e.g. "toggle" for booleans.
+     */
+    static T parse(const std::string& source, T const* relativeTo);
+
+    /** Return a user-friendly string representation */
+    static std::string str(T payload) { return std::to_string(payload); }
+};
+
+// Integers
+template<>
+inline int Converter<int>::parse(const std::string &payload, int const*) {
+    return std::stoi(payload);
+}
+template<>
+inline unsigned long Converter<unsigned long>::parse(const std::string &payload, unsigned long const*) {
+    return std::stoul(payload);
+}
+
+// Booleans
+template<>
+inline std::string Converter<bool>::str(bool payload) {
+    return { payload ? "true" : "false" };
+}
+template<>
+inline bool Converter<bool>::parse(const std::string &payload, bool const* previous) {
+    std::set<std::string> t = {"true", "on", "1"};
+    std::set<std::string> f = {"false", "off", "0"};
+    if (f.find(payload) != f.end())
+        return false;
+    else if (t.find(payload) != t.end())
+        return true;
+    else if (payload == "toggle" && previous)
+        return !*previous;
+    else throw std::invalid_argument(
+            previous
+            ? "only on/off/true/false/toggle are valid booleans"
+            : "only on/off/true/false are valid booleans");
+}
+
+// Strings
+template<>
+inline std::string Converter<std::string>::str(std::string payload) { return payload; }
+template<>
+inline std::string Converter<std::string>::parse(const std::string &payload, std::string const*) {
+    return payload;
+}
+
+// Colors
+template<>
+inline std::string Converter<Color>::str(Color payload) { return payload.str(); }
+
+template<>
+inline Color Converter<Color>::parse(const std::string &payload, Color const*) {
+    Color new_color;
+    std::string msg = Color::fromStr(payload, new_color);
+    if (msg != "") throw std::invalid_argument(msg);
+    return new_color;
+}
 
 #endif
