873a68090e6e27d7bce20b3e3f884ed5433f4590
Daniel Danner <dnnr@users.noreply.github.com>
2018-11-25 23:43:57 +0100

    Restructure and extend monitor tests (#169)
    * Split up monitor tests by individual concerns
    
    * Add tests for failing remove_monitor calls

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -1,14 +1,46 @@
-
-def test_add_remove_monitor(hlwm):
+def test_default_monitor(hlwm):
     assert hlwm.get_attr('monitors.count') == '1'
     assert hlwm.get_attr('monitors.focus.name') == ''
-    tag_name = hlwm.get_attr('monitors.focus.tag')
-    hlwm.call('add', 'bla')
-    hlwm.call('add_monitor', '800x600+40+40', 'bla', 'new_mon')
-    assert hlwm.get_attr('monitors.count') == '2'
-    hlwm.call('remove_monitor', 0)
-    assert hlwm.get_attr('monitors.count') == '1'
-    assert hlwm.get_attr('monitors.focus.name') == 'new_mon'
-    hlwm.call('add_monitor', '800x600+40+40', '', 'blub')
-    assert hlwm.get_attr('monitors.1.tag') == tag_name
 
+
+def test_add_monitor_requires_unfocused_tag(hlwm):
+    call = hlwm.call('add_monitor', '800x600+40+40', 'default', 'monitor2', check=False)
+
+    assert call.returncode != 0
+    assert hlwm.get_attr('monitors.count') == '1'
+    assert hlwm.get_attr('monitors.focus.name') == ''
+
+
+def test_add_monitor(hlwm):
+    hlwm.call('add', 'tag2')
+
+    hlwm.call('add_monitor', '800x600+40+40', 'tag2', 'monitor2')
+
+    assert hlwm.get_attr('monitors.count') == '2'
+    assert hlwm.get_attr('monitors.1.name') == 'monitor2'
+
+
+def test_remove_monitor(hlwm):
+    hlwm.call('add', 'tag2')
+    hlwm.call('add_monitor', '800x600+40+40', 'tag2', 'monitor2')
+
+    hlwm.call('remove_monitor', '0')
+
+    assert hlwm.get_attr('monitors.count') == '1'
+    assert hlwm.get_attr('monitors.focus.name') == 'monitor2'
+
+
+def test_cannot_remove_nonexistant_monitor(hlwm):
+    call = hlwm.call('remove_monitor', '1', check=False)
+
+    assert call.returncode != 0
+    assert call.stderr.endswith(' not found!\n')
+    assert hlwm.get_attr('monitors.count') == '1'
+
+
+def test_cannot_remove_last_monitor(hlwm):
+    call = hlwm.call('remove_monitor', '0', check=False)
+
+    assert call.returncode != 0
+    assert call.stderr.endswith(' last monitor\n')
+    assert hlwm.get_attr('monitors.count') == '1'
