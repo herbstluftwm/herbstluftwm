91c08504af732ebe3dac7519c402975930813171
Daniel Danner <daniel@danner.de>
2018-11-28 23:51:29 +0100

    Add tests for all failing add_monitor invocations

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -20,6 +20,59 @@ def test_add_monitor(hlwm):
     assert hlwm.get_attr('monitors.1.name') == 'monitor2'
 
 
+def test_cannot_add_monitor_without_free_tag(hlwm):
+    call = hlwm.call('add_monitor', '800x600+40+40', check=False)
+
+    assert call.returncode != 0
+    assert call.stderr.endswith(' not enough free tags\n')
+    assert hlwm.get_attr('monitors.count') == '1'
+
+
+def test_cannot_add_monitor_with_nonexistent_tag(hlwm):
+    call = hlwm.call('add_monitor', '800x600+40+40', 'derp', check=False)
+
+    assert call.returncode != 0
+    assert call.stderr.endswith(' does not exist\n')
+    assert hlwm.get_attr('monitors.count') == '1'
+
+
+def test_cannot_add_monitor_with_already_viewed_tag(hlwm):
+    hlwm.call('add', 'tag2')
+    call = hlwm.call('add_monitor', '800x600+40+40', 'default', check=False)
+
+    assert call.returncode != 0
+    assert call.stderr.endswith(' already being viewed on a monitor\n')
+    assert hlwm.get_attr('monitors.count') == '1'
+
+
+def test_cannot_add_monitor_with_numeric_name(hlwm):
+    hlwm.call('add', 'tag2')
+    call = hlwm.call('add_monitor', '800x600+40+40', 'tag2', '123foo', check=False)
+
+    assert call.returncode != 0
+    assert call.stderr.endswith(' may not start with a number\n')
+    assert hlwm.get_attr('monitors.count') == '1'
+
+
+def test_cannot_add_monitor_with_empty_name(hlwm):
+    hlwm.call('add', 'tag2')
+    call = hlwm.call('add_monitor', '800x600+40+40', 'tag2', '', check=False)
+
+    assert call.returncode != 0
+    assert call.stderr.endswith(' empty monitor name is not permitted\n')
+    assert hlwm.get_attr('monitors.count') == '1'
+
+
+def test_cannot_add_monitor_with_existing_name(hlwm):
+    hlwm.call('rename_monitor', '0', 'mon1')
+    hlwm.call('add', 'tag2')
+    call = hlwm.call('add_monitor', '800x600+40+40', 'tag2', 'mon1', check=False)
+
+    assert call.returncode != 0
+    assert call.stderr.endswith(' same name already exists\n')
+    assert hlwm.get_attr('monitors.count') == '1'
+
+
 def test_remove_monitor(hlwm):
     hlwm.call('add', 'tag2')
     hlwm.call('add_monitor', '800x600+40+40', 'tag2', 'monitor2')
