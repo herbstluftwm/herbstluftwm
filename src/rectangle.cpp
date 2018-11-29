314e14c80d7cbe9279733f10930578660e1ee6f3
Johannes Jordan <git@lanrules.de>
2018-11-29 10:27:49 +0100

    Don't use RectList
    first part in effort to remove RectList altogether

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -5,13 +5,12 @@
 #include "ipc-protocol.h"
 #include <glib.h>
 
-static bool rects_intersect(RectList* m1, RectList* m2) {
-    Rectangle *r1 = &m1->rect, *r2 = &m2->rect;
-    bool is = TRUE;
-    is = is && intervals_intersect(r1->x, r1->x + r1->width,
-                                   r2->x, r2->x + r2->width);
-    is = is && intervals_intersect(r1->y, r1->y + r1->height,
-                                   r2->y, r2->y + r2->height);
+static bool rects_intersect(const Rectangle &a, const Rectangle &b) {
+    bool is = true;
+    is = is && intervals_intersect(a.x, a.x + a.width,
+                                   b.x, b.x + b.width);
+    is = is && intervals_intersect(a.y, a.y + a.height,
+                                   b.y, b.y + b.height);
     return is;
 }
 
@@ -80,7 +79,7 @@ RectList* reclist_insert_disjoint(RectList* head, RectList* element) {
         // if the list is empty, then intersection-free insertion is trivial
         element->next = nullptr;
         return element;
-    } else if (!rects_intersect(head, element)) {
+    } else if (!rects_intersect(head->rect, element->rect)) {
         head->next = reclist_insert_disjoint(head->next, element);
         return head;
     } else {
