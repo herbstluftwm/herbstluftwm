4e0881edfd4859e65743a74bb9884e046c0581c7
Thorsten Wißmann <edu@thorsten-wissmann.de>
2018-11-30 16:36:27 +0100

    Add tag attributes
    Add the tag attributes
    
        frame_count, client_count, curframe_windex, curframe_wcount
    
    with the same meaning as in master. This allows writing detailed test
    cases on the tiling algorithm.

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -12,11 +12,17 @@ def test_add_tag(hlwm):
 
 def test_move_focused_client_to_new_tag(hlwm, create_client):
     hlwm.callstr('add foobar')
+    assert hlwm.get_attr('tags.0.client_count') == '0'
+    assert hlwm.get_attr('tags.1.client_count') == '0'
+
     create_client()
+    assert hlwm.get_attr('tags.0.client_count') == '1'
+    assert hlwm.get_attr('tags.1.client_count') == '0'
 
     hlwm.callstr('move foobar')
+    assert hlwm.get_attr('tags.0.client_count') == '0'
+    assert hlwm.get_attr('tags.1.client_count') == '1'
 
-    # TODO: Assert that foobar now has 1 client
     # TODO: Assert that winid is now in foobar
 
 def test_merge_tag_into_another_tag(hlwm, create_client):
