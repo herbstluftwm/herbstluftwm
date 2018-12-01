39135afd91d5eda62a8e09291d771a5ddab2d5bf
Thorsten Wißmann <edu@thorsten-wissmann.de>
2018-12-01 17:17:33 +0100

    Split big layout test into smaller pieces

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -1,43 +1,48 @@
+import pytest
 
-def explode_and_remove(hlwm,create_client,number_clients):
-    assert number_clients >= 2, "explode behaves as auto for one client"
+@pytest.mark.parametrize("number_clients", [0,1,2])
+def test_single_frame_layout(hlwm,create_client,number_clients):
     for i in range(0, number_clients):
         create_client()
     assert hlwm.get_attr('tags.0.curframe_windex') == '0'
+    assert int(hlwm.get_attr('tags.0.client_count')) == number_clients
     assert int(hlwm.get_attr('tags.0.curframe_wcount')) == number_clients
-    assert hlwm.get_attr('tags.0.frame_count') == '1'
+
+@pytest.mark.parametrize("number_clients", [2,3])
+def test_explode(hlwm,create_client,number_clients):
+    assert number_clients >= 2, "explode behaves as auto for one client"
+    for i in range(0, number_clients):
+        create_client()
     hlwm.callstr('split explode')
-    # half of the clients in the upper frame, the other in the lower
     assert hlwm.get_attr('tags.0.curframe_windex') == '0'
     number_upper = (number_clients + 1) // 2
     assert int(hlwm.get_attr('tags.0.curframe_wcount')) == number_upper
     assert int(hlwm.get_attr('tags.0.client_count')) == number_clients
     assert hlwm.get_attr('tags.0.frame_count') == '2'
-    # after calling 'focus down' for number_upper-times, the focus switches to
-    # the lower frame
-    for i in range(0, number_upper):
-        assert int(hlwm.get_attr('tags.0.curframe_windex')) == i
-        assert int(hlwm.get_attr('tags.0.curframe_wcount')) == number_upper
-        hlwm.callstr('focus down')
-    # after the last 'focus down' we fall into the other frame
-    assert hlwm.get_attr('tags.0.curframe_windex') == '0'
-    assert int(hlwm.get_attr('tags.0.curframe_wcount')) \
-            == number_clients - number_upper
-    assert hlwm.get_attr('tags.0.frame_count') == '2'
 
-    hlwm.call('remove')
+@pytest.mark.parametrize("number_clients", [0,1,4])
+def test_remove(hlwm,create_client,number_clients):
+    for i in range(0, number_clients):
+        create_client()
+    hlwm.callstr('split explode')
+    hlwm.callstr('remove')
     assert int(hlwm.get_attr('tags.0.curframe_wcount')) == number_clients
     assert int(hlwm.get_attr('tags.0.client_count')) == number_clients
     assert hlwm.get_attr('tags.0.frame_count') == '1'
     # TODO: reasonably handle focus, e.g. to have
     #assert hlwm.get_attr('tags.0.curframe_windex') == '2'
 
-def test_explode_remove_2(hlwm,create_client):
-    explode_and_remove(hlwm,create_client, 2)
-
-def test_explode_remove_3(hlwm,create_client):
-    explode_and_remove(hlwm,create_client, 3)
-
-def test_explode_remove_4(hlwm,create_client):
-    explode_and_remove(hlwm,create_client, 4)
+@pytest.mark.parametrize("number_clients", [3,4])
+def test_focus_wrap(hlwm,create_client,number_clients):
+    assert number_clients >= 2, "explode behaves as auto for one client"
+    for i in range(0, number_clients):
+        create_client()
+    # TODO: create the layout explicitly once we have the 'load' command again
+    #       instead of calling explode
+    hlwm.callstr('split explode')
+    for i in range(0, number_clients):
+        expected_idx = i % ((number_clients + 1) // 2)
+        assert int(hlwm.get_attr('tags.0.curframe_windex')) == expected_idx
+        if i < number_clients - 1:
+            hlwm.callstr('focus down')
 
