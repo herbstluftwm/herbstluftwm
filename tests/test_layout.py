a0cdf7fbabbd6787c5339af8b5eed4e9176f15ba
Daniel Danner <daniel@danner.de>
2018-12-02 17:32:43 +0100

    Use new fixtures in layout tests

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -1,48 +1,50 @@
 import pytest
 
-@pytest.mark.parametrize("number_clients", [0,1,2])
-def test_single_frame_layout(hlwm,create_client,number_clients):
-    for i in range(0, number_clients):
-        create_client()
+@pytest.mark.parametrize("running_clients_num", [0, 1, 2])
+def test_single_frame_layout(hlwm, running_clients, running_clients_num):
     assert hlwm.get_attr('tags.0.curframe_windex') == '0'
-    assert int(hlwm.get_attr('tags.0.client_count')) == number_clients
-    assert int(hlwm.get_attr('tags.0.curframe_wcount')) == number_clients
+    assert int(hlwm.get_attr('tags.0.client_count')) == running_clients_num
+    assert int(hlwm.get_attr('tags.0.curframe_wcount')) == running_clients_num
 
-@pytest.mark.parametrize("number_clients", [2,3])
-def test_explode(hlwm,create_client,number_clients):
-    assert number_clients >= 2, "explode behaves as auto for one client"
-    for i in range(0, number_clients):
-        create_client()
+
+def test_single_frame_layout_three(hlwm, create_clients):
+    create_clients(3)
+    assert hlwm.get_attr('tags.0.curframe_windex') == '0'
+    assert int(hlwm.get_attr('tags.0.client_count')) == 3
+    assert int(hlwm.get_attr('tags.0.curframe_wcount')) == 3
+
+
+@pytest.mark.parametrize("running_clients_num", [2, 3])
+def test_explode(hlwm, running_clients, running_clients_num):
+    assert running_clients_num >= 2, "explode behaves as auto for one client"
     hlwm.callstr('split explode')
     assert hlwm.get_attr('tags.0.curframe_windex') == '0'
-    number_upper = (number_clients + 1) // 2
+    number_upper = (running_clients_num + 1) // 2
     assert int(hlwm.get_attr('tags.0.curframe_wcount')) == number_upper
-    assert int(hlwm.get_attr('tags.0.client_count')) == number_clients
+    assert int(hlwm.get_attr('tags.0.client_count')) == running_clients_num
     assert hlwm.get_attr('tags.0.frame_count') == '2'
 
-@pytest.mark.parametrize("number_clients", [0,1,4])
-def test_remove(hlwm,create_client,number_clients):
-    for i in range(0, number_clients):
-        create_client()
+
+@pytest.mark.parametrize("running_clients_num", [0, 1, 4])
+def test_remove(hlwm, running_clients, running_clients_num):
     hlwm.callstr('split explode')
     hlwm.callstr('remove')
-    assert int(hlwm.get_attr('tags.0.curframe_wcount')) == number_clients
-    assert int(hlwm.get_attr('tags.0.client_count')) == number_clients
+    assert int(hlwm.get_attr('tags.0.curframe_wcount')) == running_clients_num
+    assert int(hlwm.get_attr('tags.0.client_count')) == running_clients_num
     assert hlwm.get_attr('tags.0.frame_count') == '1'
     # TODO: reasonably handle focus, e.g. to have
     #assert hlwm.get_attr('tags.0.curframe_windex') == '2'
 
-@pytest.mark.parametrize("number_clients", [3,4])
-def test_focus_wrap(hlwm,create_client,number_clients):
-    assert number_clients >= 2, "explode behaves as auto for one client"
-    for i in range(0, number_clients):
-        create_client()
+
+@pytest.mark.parametrize("running_clients_num", [3, 4])
+def test_focus_wrap(hlwm,running_clients,running_clients_num):
+    assert running_clients_num >= 2, "explode behaves as auto for one client"
     # TODO: create the layout explicitly once we have the 'load' command again
     #       instead of calling explode
     hlwm.callstr('split explode')
-    for i in range(0, number_clients):
-        expected_idx = i % ((number_clients + 1) // 2)
+    for i in range(0, running_clients_num):
+        expected_idx = i % ((running_clients_num + 1) // 2)
         assert int(hlwm.get_attr('tags.0.curframe_windex')) == expected_idx
-        if i < number_clients - 1:
+        if i < running_clients_num - 1:
             hlwm.callstr('focus down')
 
