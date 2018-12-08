c1cabbcf77ffc2177618b25a50af677237afc188
Daniel Danner <daniel@danner.de>
2018-12-08 17:15:41 +0100

    Change call interface to optimize for common cases

--- tests/test_root_commands.py
+++ tests/test_root_commands.py
@@ -1,28 +1,30 @@
-
 def test_attr_cmd(hlwm):
     assert hlwm.get_attr('monitors.focus.name') == ''
     hlwm.call('attr')
-    hlwm.callstr('attr tags')
-    hlwm.callstr('attr tags.')
-    hlwm.callstr('attr tags.count')
-    assert hlwm.call('attr', 'tags.count').stdout == hlwm.get_attr('tags.count')
-    hlwm.call_xfail('attr', 'tags.co')
+    hlwm.call('attr tags')
+    hlwm.call('attr tags.')
+    hlwm.call('attr tags.count')
+    assert hlwm.call('attr tags.count').stdout == hlwm.get_attr('tags.count')
+    hlwm.call_xfail('attr tags.co')
+
 
 def test_object_tree(hlwm):
     t1 = hlwm.call('object_tree').stdout.splitlines()
-    t2 = hlwm.call('object_tree', 'theme.').stdout.splitlines()
-    t3 = hlwm.call('object_tree', 'theme.tiling.').stdout.splitlines()
+    t2 = hlwm.call('object_tree theme.').stdout.splitlines()
+    t3 = hlwm.call('object_tree theme.tiling.').stdout.splitlines()
     assert len(t1) > len(t2)
     assert len(t2) > len(t3)
+
 
 def test_sprintf(hlwm):
     cnt = hlwm.get_attr('tags.count')
     wmname = hlwm.get_attr('settings.wmname')
-    printed = hlwm.callstr('sprintf X %s/%s tags.count settings.wmname echo X').stdout
-    hlwm.call_xfail('sprintf X %s/%s tags.count settings.wmname')
-    hlwm.call_xfail('sprintf X %s/%s tags.count')
+    printed = hlwm.call('sprintf X %s/%s tags.count settings.wmname echo X').stdout
+    hlwm.call_xfail('"sprintf X %s/%s tags.count settings.wmname"')
+    hlwm.call_xfail('"sprintf X %s/%s tags.count"')
     assert printed == cnt + '/' + wmname + '\n'
-    assert '%\n' == hlwm.callstr('sprintf X %% echo X').stdout
+    assert '%\n' == hlwm.call('sprintf X %% echo X').stdout
+
 
 def test_disjoint_rects(hlwm):
     # test the example from the manpage
@@ -33,5 +35,5 @@ def test_disjoint_rects(hlwm):
         '300x150+600+250',
         '600x250+300+400',
         '')) # trailing newline
-    response = hlwm.callstr('disjoin_rects 600x400+0+0 600x400+300+250').stdout
+    response = hlwm.call('disjoin_rects 600x400+0+0 600x400+300+250').stdout
     assert response == expected
--- tests/test_rules.py
+++ tests/test_rules.py
@@ -9,62 +9,62 @@ def test_list_rules_empty_by_default(hlwm):
 
 
 def test_add_simple_rule(hlwm):
-    hlwm.call('rule', 'class=Foo', 'tag=bar')
+    hlwm.call('rule class=Foo tag=bar')
 
     rules = hlwm.call('list_rules')
     assert rules.stdout == 'label=0\tclass=Foo\ttag=bar\t\n'
 
 
 def test_add_labeled_rule(hlwm):
-    hlwm.call('rule', 'label=mylabel', 'class=Foo', 'tag=bar')
+    hlwm.call('rule label=mylabel class=Foo tag=bar')
 
     rules = hlwm.call('list_rules')
     assert rules.stdout == 'label=mylabel\tclass=Foo\ttag=bar\t\n'
 
 
 def test_cannot_add_rule_with_empty_label(hlwm):
-    call = hlwm.call_xfail('rule', 'label=', 'class=Foo', 'tag=bar')
+    call = hlwm.call_xfail('rule label= class=Foo tag=bar')
 
     assert call.stderr == 'rule: Rule label cannot be empty'
 
 
 def test_cannot_use_tilde_operator_for_rule_label(hlwm):
-    call = hlwm.call_xfail('rule', 'label~bla', 'class=Foo', 'tag=bar')
+    call = hlwm.call_xfail('rule label~bla class=Foo tag=bar')
 
     assert call.stderr == 'rule: Unknown rule label operation "~"\n'
 
 
 @pytest.mark.parametrize('method', ['-F', '--all'])
 def test_remove_all_rules(hlwm, method):
-    hlwm.call('rule', 'class=Foo', 'tag=bar')
-    hlwm.call('rule', 'label=labeled', 'class=Bork', 'tag=baz')
+    hlwm.call('rule class=Foo tag=bar')
+    hlwm.call('rule label=labeled class=Bork tag=baz')
 
-    hlwm.call('unrule', method)
+    hlwm.call(['unrule', method])
 
     rules = hlwm.call('list_rules')
     assert rules.stdout == ''
 
 
 def test_remove_simple_rule(hlwm):
-    hlwm.call('rule', 'class=Foo', 'tag=bar')
+    hlwm.call('rule class=Foo tag=bar')
 
-    hlwm.call('unrule', '0')
+    hlwm.call('unrule 0')
 
     rules = hlwm.call('list_rules')
     assert rules.stdout == ''
 
 
 def test_remove_labeled_rule(hlwm):
-    hlwm.call('rule', 'label=blah', 'class=Foo', 'tag=bar')
+    hlwm.call('rule label=blah class=Foo tag=bar')
 
-    hlwm.call('unrule', 'blah')
+    hlwm.call('unrule blah')
 
     rules = hlwm.call('list_rules')
     assert rules.stdout == ''
 
 
 def test_singleuse_rule_disappears_after_matching(hlwm):
-    hlwm.call('rule', 'once', 'hook=dummy_hook')
+    hlwm.call('rule once hook=dummy_hook')
 
     hlwm.create_client()
 
@@ -74,13 +74,13 @@ def test_singleuse_rule_disappears_after_matching(hlwm):
 @pytest.mark.parametrize('rules_count', [1, 2, 10])
 def test_rule_labels_are_not_reused(hlwm, rules_count):
     # First add some rules and remove them again
-    for i in map(str, range(rules_count)):
-        hlwm.call('rule', 'class=Foo' + i, 'tag=bar' + i)
-    for i in map(str, range(rules_count)):
-        hlwm.call('unrule', i)
+    for i in range(rules_count):
+        hlwm.call('rule class=Foo{0} tag=bar{0}'.format(i))
+    for i in range(rules_count):
+        hlwm.call('unrule {}'.format(i))
 
     # Add back a single new rule
-    hlwm.callstr('rule class=meh tag=moo')
+    hlwm.call('rule class=meh tag=moo')
 
     # Remaining rule has a high label number (not zero)
     rules = hlwm.call('list_rules')
