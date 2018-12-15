448ec40e10214414354fdc0fec293ab045b3ffa5
Daniel Danner <daniel@danner.de>
2018-12-15 18:04:45 +0100

    Make test match impl

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -145,7 +145,7 @@ def test_rule_labels_are_not_reused(hlwm, rules_count):
 def test_cannot_use_invalid_operator_for_consequence(hlwm):
     call = hlwm.call_xfail('rule class=Foo tag~bar')
 
-    assert call.stderr == 'rule: Unknown rule consequence operation "~"\n'
+    assert call.stderr == 'rule: Operator ~ not valid for consequence "tag"\n'
 
 
 @pytest.mark.parametrize('rules_count', [1, 2, 10])
