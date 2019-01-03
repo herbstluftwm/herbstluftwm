4edc0c770d6f3bc2f5a6f158db4dd6c3db312627
Thorsten Wißmann <edu@thorsten-wissmann.de>
2019-01-03 19:20:50 +0100

    Add FIXME

--- tests/test_completion.py
+++ tests/test_completion.py
@@ -25,5 +25,6 @@ commands_without_input = shlex.split(
 
 @pytest.mark.parametrize('name', commands_without_input)
 def test_inputless_commands(hlwm, name):
+    # FIXME: document exit code. Here, 7 = NO_PARAMETER_EXPECTED
     assert hlwm.call_xfail_no_output('complete 1 ' + name) \
         .returncode == 7
