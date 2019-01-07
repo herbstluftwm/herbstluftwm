de1ca7fc1b057b69d4b6d05c51ec0b64db1c4e76
Daniel Danner <dnnr@users.noreply.github.com>
2019-01-07 21:49:38 +0100

    CMake: Use 4 spaces for indentation, add modelines (#255)

--- cmake/behavior.cmake
+++ cmake/behavior.cmake
@@ -1,6 +1,6 @@
 # old versions don't support our min.required string, manually up the policy
 if(${CMAKE_VERSION} VERSION_LESS 3.12)
-	cmake_policy(VERSION ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION})
+    cmake_policy(VERSION ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION})
 endif()
 
 # write all executables to root of build directory
@@ -9,8 +9,10 @@ set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
 # set default build type to RELEASE
 # from cmake devs, https://blog.kitware.com/cmake-and-the-default-build-type/
 if (NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-	set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build." FORCE)
-	# Set the possible values of build type for cmake-gui
-	set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
-		"Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+    set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build." FORCE)
+    # Set the possible values of build type for cmake-gui
+    set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
+        "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
 endif()
+
+# vim: et:ts=4:sw=4
