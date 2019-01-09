dfda27e777e77804b94714b8e783d476bec933ad
Daniel Danner <daniel@danner.de>
2019-01-09 19:34:32 +0100

    Add tests for unknown modifier/keysym

--- tests/test_root_commands.py
+++ tests/test_root_commands.py
@@ -139,3 +139,15 @@ def test_list_keybinds(hlwm):
     keybinds = hlwm.call('list_keybinds')
 
     assert keybinds.stdout == 'Mod1+x\tquit\n'
+
+
+def test_keybind_unknown_modifier(hlwm):
+    call = hlwm.call_xfail('keybind Moep1-x quit')
+
+    assert call.stderr == 'keybind: No such KeySym/modifier\n'
+
+
+def test_keybind_unknown_keysym(hlwm):
+    call = hlwm.call_xfail('keybind Mod1-_ quit')
+
+    assert call.stderr == 'keybind: No such KeySym/modifier\n'
