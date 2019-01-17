74055ab3ed2a34f715fe5908184c8347f400972a
Daniel Danner <dnnr@users.noreply.github.com>
2019-01-17 20:51:20 +0100

    Extend test suite for key bindings (#287)

--- tests/test_keybind.py
+++ tests/test_keybind.py
@@ -2,8 +2,9 @@ import pytest
 import subprocess
 
 
-def test_list_keybinds(hlwm):
-    hlwm.call('keybind Mod1-x quit')
+@pytest.mark.parametrize('separator', ['-', '+'])
+def test_list_keybinds(hlwm, separator):
+    hlwm.call(f'keybind Mod1{separator}x quit')
 
     keybinds = hlwm.call('list_keybinds')
 
@@ -20,6 +21,35 @@ def test_keybind_unknown_keysym(hlwm):
     call = hlwm.call_xfail('keybind Mod1-_ quit')
 
     assert call.stderr == 'keybind: No such KeySym/modifier\n'
+
+
+def test_replace_keybind(hlwm):
+    hlwm.call(f'keybind Mod1+x quit')
+
+    hlwm.call(f'keybind Mod1+x cycle')
+
+    assert hlwm.call('list_keybinds').stdout == 'Mod1+x\tcycle\n'
+
+
+def test_keyunbind_specific_binding(hlwm):
+    hlwm.call('keybind Mod1+x quit')
+    hlwm.call('keybind Mod1+y cycle')
+    hlwm.call('keybind Mod1+z close')
+
+    unbind = hlwm.call('keyunbind Mod1+y')
+
+    assert unbind.stdout == ''
+    assert hlwm.call('list_keybinds').stdout == 'Mod1+x\tquit\nMod1+z\tclose\n'
+
+
+@pytest.mark.parametrize('method', ['-F', '--all'])
+def test_keyunbind_all(hlwm, method):
+    hlwm.call('keybind Mod1+x quit')
+
+    unbind = hlwm.call(['keyunbind', method])
+
+    assert unbind.stdout == ''
+    assert hlwm.call('list_keybinds').stdout == ''
 
 
 def test_trigger_single_key_binding(hlwm):
