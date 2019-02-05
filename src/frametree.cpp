bcb17766e3ed28c3f1478447c4150291e8605743
Thorsten Wißmann <edu@thorsten-wissmann.de>
2019-02-05 12:48:15 +0100

    Dummy-use members of FrameTree

--- src/frametree.cpp
+++ src/frametree.cpp
@@ -7,6 +7,8 @@ FrameTree::FrameTree(HSTag* tag, Settings* settings)
     , settings_(settings)
 {
     root_ = std::make_shared<HSFrameLeaf>(tag, settings, std::shared_ptr<HSFrameSplit>());
+    (void) tag_;
+    (void) settings_;
 }
 
 void FrameTree::foreachClient(std::function<void(Client*)> action)
