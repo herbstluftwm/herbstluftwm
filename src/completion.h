3008bb287ddaa5dfbb27657d7c21d77ce624207d
Daniel Danner <dnnr@users.noreply.github.com>
2019-02-10 15:48:46 +0100

    Implement completion for "keybind" in KeyManager (#327)
    * Clean up responsibilities of KeyCombo helpers
    
    Dealing with the structure of a KeyCombo (i.e., last token is keysym,
    others are modifiers) is now part of fromString(). This makes the
    renamed helpers for parsing modifiers and keysyms more flexible for
    other use cases (completion in particular).
    
    The helper for splitting a string into tokens is made public for future
    use in completion code in KeyManager.
    
    As a small cleanup, a local variable in mouse.cpp is renamed from
    "string" to "str" to avoid shadowing the type.
    
    * Simplify loop
    
    * Turn KEY_COMBI_SEPARATORS into member of KeyCombo
    
    * Remove unused declarations from key module
    
    * WiP: Implement completion for keybind
    
    * Comment
    
    * Tune join_strings, use in ArgList
    
    * Use join_strings in HookManager
    
    * Fix command completion: Use Complete::none()
    
    * Implement command completion for keybind
    
    * Re-enable test
    
    * Remove dead code
    
    * Fix command completion properly

--- src/completion.h
+++ src/completion.h
@@ -62,18 +62,20 @@ public:
 
     Completion shifted(size_t offset) const;
 
-    /** grand access to private members as long as Commands::complete is still
-     * wraper around complete_against_commands.
+    /** Grants access to private members as long as Commands::complete is still
+     * wrapper around complete_against_commands.
      */
     friend void Commands::complete(Completion& completion);
+
+    void completeCommands(size_t offset);
 private:
     /** The intended use is to pass the completion state as the reference and
      * to return possible completions via this Completion object. This is why
      * the operator= and the copy constructor are private. It ensures that a
      * completion object is not accidentally duplicated.
      */
-    Completion(const Completion& other);
     void operator=(const Completion& other);
+    Completion(const Completion& other);
 
     std::string escape(const std::string& str);
 
