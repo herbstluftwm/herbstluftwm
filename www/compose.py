b50461d252a9eb4e60d124e5598e50190d227bf1
Simon Kuhnle <simon@blarzwurst.de>
2019-04-08 18:56:32 +0200

    Fix compose.py, too

--- www/compose.py
+++ www/compose.py
@@ -2,12 +2,11 @@
 
 import sys
 import datetime
-import types
 from collections import OrderedDict
 
 tabs = OrderedDict([
     ("Overview", OrderedDict([
-        ("index",""),
+        ("index", ""),
     ])),
     ("Documentation", OrderedDict([
         ("news", "News"),
@@ -42,10 +41,10 @@ for title, subpages in tabs.iteritems():
 
 curtab = page2tab[name]
 
-#====~===~=========~==
+# ====~===~=========~==
 # Header
-#====~===~=========~==
-print """\
+# ====~===~=========~==
+print("""\
 <html>
  <head>
   <link rel="stylesheet" href="main.css" type="text/css" />
@@ -64,14 +63,14 @@ print """\
        </div>
      </div>
     </div>
-   </div>""".format(title=windowtitle)
+   </div>""").format(title=windowtitle)
 
-#====~===~=========~==
+# ====~===~=========~==
 # Navigation bar
-#====~===~=========~==
+# ====~===~=========~==
 
-print """\
-    <ul id="navigationbar">"""
+print("""\
+    <ul id="navigationbar">""")
 
 for title, subpages in tabs.iteritems():
     classstring = "notab"
@@ -81,60 +80,58 @@ for title, subpages in tabs.iteritems():
         trg = subpages
     else:
         trg = subpages.keys()[0] + ".html"
-    print '<li class="{cls}"><a href="{target}">{title}</a></li>'.format(
-        cls = classstring,
-        target = trg,
-        title = title)
+    print('<li class="{cls}"><a href="{target}">{title}</a></li>').format(
+        cls=classstring,
+        target=trg,
+        title=title)
 
 
-print """\
+print("""\
     </ul>\
     <div class="tabbarseparator"></div>
-"""
+""")
 
 subpages = tabs[page2tab[name]]
 
 if len(subpages) > 1:
-    print '<div class="subpagebar">'
+    print('<div class="subpagebar">')
     for basename, title in subpages.iteritems():
         if basename == name:
             cls = "subpagecur subpage"
         else:
             cls = "subpage"
-        print '<span class="{cls}">'.format(cls = cls)
-        print '<a href="{url}">{title}</a></span>'.format(
-            url = basename + ".html",title = title)
-    print "</div>"
+        print('<span class="{cls}">'.format(cls=cls))
+        print('<a href="{url}">{title}</a></span>').format(
+            url=basename + ".html", title=title)
+    print("</div>")
 
-
-
-print """\
+print("""\
     <div id="content">\
-"""
+    """)
 
 # possibly table of contents:
 try:
-	print open(toc).read()
+    print(open(toc).read())
 except IOError:
-	# no toc file
-	print "<!-- no toc file present -->"
-print open(filename).read()
+    #  no toc file
+    print("<!-- no toc file present -->")
+print(open(filename).read())
 
 
-print """\
+print("""\
     <div class="footer">
       Generated on {date}
     </div>
-""".format(date=datetime.datetime.now().strftime('%Y-%m-%d at %H:%M:%S %Z'))
+""").format(date=datetime.datetime.now().strftime('%Y-%m-%d at %H:%M:%S %Z'))
 
-#====~===~=========~==
+# ====~===~=========~==
 # Footer
-#====~===~=========~==
-print """\
+# ====~===~=========~==
+print("""\
    </div>
   </div>
  </body>
 </html>
-"""
+""")
 
 # vim: noet
