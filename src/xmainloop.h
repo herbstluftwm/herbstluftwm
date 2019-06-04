cb225075dad9e8671008ec0bf1369c1bc2663414
Thorsten Wißmann <edu@thorsten-wissmann.de>
2019-06-04 20:30:44 +0200

    Include Xlib.h such that iwyu does not complain anymore

--- src/xmainloop.h
+++ src/xmainloop.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <X11/X.h>
+#include <X11/Xlib.h>
 
 class Root;
-union _XEvent;
 class XConnection;
 
 class XMainLoop {
@@ -13,28 +13,28 @@ public:
     //! quit the main loop as soon as possible
     void quit();
 private:
-    using EventHandler = void (XMainLoop::*)(union _XEvent*);
+    using EventHandler = void (XMainLoop::*)(XEvent*);
     // members
     XConnection& X_;
     Root* root_;
     bool aboutToQuit_;
     EventHandler handlerTable_[LASTEvent];
     // event handlers
-    void buttonpress(union _XEvent* event);
-    void buttonrelease(union _XEvent* event);
-    void clientmessage(union _XEvent* event);
-    void createnotify(union _XEvent* event);
-    void configurerequest(union _XEvent* event);
-    void configurenotify(union _XEvent* event);
-    void destroynotify(union _XEvent* event);
-    void enternotify(union _XEvent* event);
-    void expose(union _XEvent* event);
-    void focusin(union _XEvent* event);
-    void keypress(union _XEvent* event);
-    void mappingnotify(union _XEvent* event);
-    void motionnotify(union _XEvent* event);
-    void mapnotify(union _XEvent* event);
-    void maprequest(union _XEvent* event);
-    void propertynotify(union _XEvent* event);
-    void unmapnotify(union _XEvent* event);
+    void buttonpress(XEvent* event);
+    void buttonrelease(XEvent* event);
+    void clientmessage(XEvent* event);
+    void createnotify(XEvent* event);
+    void configurerequest(XEvent* event);
+    void configurenotify(XEvent* event);
+    void destroynotify(XEvent* event);
+    void enternotify(XEvent* event);
+    void expose(XEvent* event);
+    void focusin(XEvent* event);
+    void keypress(XEvent* event);
+    void mappingnotify(XEvent* event);
+    void motionnotify(XEvent* event);
+    void mapnotify(XEvent* event);
+    void maprequest(XEvent* event);
+    void propertynotify(XEvent* event);
+    void unmapnotify(XEvent* event);
 };
