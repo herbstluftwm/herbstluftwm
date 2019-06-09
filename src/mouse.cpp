8d1a79b2b894c0a4578ef5028b7476c722db6a23
Thorsten Wißmann <edu@thorsten-wissmann.de>
2019-06-09 16:12:25 +0200

    Fix includes

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -1,15 +1,12 @@
 #include "mouse.h"
 
-#include <X11/X.h>
 #include <cstdlib>
 
 #include "client.h"
 #include "decoration.h"
 #include "frametree.h"
-#include "keymanager.h"
 #include "layout.h"
 #include "monitor.h"
-#include "root.h"
 #include "settings.h"
 #include "tag.h"
 #include "utils.h"
