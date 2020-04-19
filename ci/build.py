7974b7ac96dc141caa34323258e1621767a08c0e
Daniel Danner <dnnr@users.noreply.github.com>
2020-04-19 11:19:01 +0200

    Fix complaints by -Wnull-dereference, enable in CI (#824)
    * Fix reasons for -Wnull-dereference, enable in CI
    
    * Add another more explicit check
    
    * Improve message
    
    * +need
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- ci/build.py
+++ ci/build.py
@@ -62,8 +62,8 @@ build_env = os.environ.copy()
 build_env.update({
     'CC': args.cc,
     'CXX': args.cxx,
-    'CFLAGS': '--coverage -Werror -Wno-error=null-dereference',
-    'CXXFLAGS': '--coverage -Werror -Wno-error=null-dereference',
+    'CFLAGS': '--coverage -Werror',
+    'CXXFLAGS': '--coverage -Werror',
 })
 
 cmake_args = [
