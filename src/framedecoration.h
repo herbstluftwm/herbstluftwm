ad1cf6d91228089cdb4acfd6e81e5791bd31955a
Thorsten Wißmann <edu@thorsten-wissmann.de>
2020-04-25 17:26:32 +0200

    Focus frames with the mouse (#864)
    This adds basic support for selecting frames with the mouse: either by
    click or by mouse hover (if focus_follows_mouse is enabled). This change
    only concerns the actual frame. If there is no frame shown (because
    always_show_frame=off) or if there is a hole in the frame (because
    frame_bg_transparent=on), then we only obtain events for the root window
    that are currently ignored. (Interpreting event coordinates on the root
    window is in the scope of another change)
    
    On the implementation level, this introduces a static map that
    transforms window ids into frame decorations (I think a 'framemanager'
    is overkill here). The focusFrame() function is added to the root
    window, because it affects the global state of hlwm (and I think
    focus_client() should go to Root at some point, too).
    
    This solves issue #20 (in principle) and adds a test case for it.
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/framedecoration.h
+++ src/framedecoration.h
@@ -2,9 +2,12 @@
 #define __HLWM_FRAME_DECORATION_H_
 
 #include <X11/X.h>
+#include <map>
+#include <memory>
 
 #include "x11-types.h"
 
+class FrameLeaf;
 class HSTag;
 class Slice;
 class Settings;
@@ -20,13 +23,18 @@ public:
 
 class FrameDecoration {
 public:
-    FrameDecoration(HSTag* tag, Settings* settings);
+    FrameDecoration(FrameLeaf& frame, HSTag* tag, Settings* settings);
     ~FrameDecoration();
     void render(const FrameDecorationData& data, bool isFocused);
     void updateVisibility(const FrameDecorationData& data, bool isFocused);
     void hide();
+    std::shared_ptr<FrameLeaf> frame();
+
+    static FrameDecoration* withWindow(Window winid);
 
 private:
+    static std::map<Window, FrameDecoration*> s_windowToFrameDecoration;
+    FrameLeaf& frame_; //! the owner of this decoration
     Window window;
     bool visible; // whether the window is visible at the moment
     bool window_transparent; // whether the window has a mask at the moment
--- tests/test_basic_events.py
+++ tests/test_basic_events.py
@@ -50,3 +50,32 @@ def test_focus_follows_mouse(hlwm, mouse, focus_follows_mouse, single_floating):
     assert c1_is_focused == focus_follows_mouse
     # stacking is unchanged
     assert test_stack.helper_get_stack_as_list(hlwm) == [c2, c1]
+
+
+@pytest.mark.parametrize("click", [True, False])
+@pytest.mark.parametrize("focus_follows_mouse", [True, False])
+def test_focus_frame_by_mouse(hlwm, mouse, click, focus_follows_mouse):
+    hlwm.call('set always_show_frame on')
+    hlwm.call('set frame_bg_transparent off')
+    hlwm.call(['set', 'focus_follows_mouse', hlwm.bool(focus_follows_mouse)])
+    layout_template = '(split horizontal:0.5:{}'
+    layout_template += ' (clients vertical:0)'
+    layout_template += ' (clients vertical:0))'
+    # a layout where the left frame is focused
+    hlwm.call(['load', layout_template.format(0)])
+    assert layout_template.format(0) == hlwm.call('dump').stdout
+    width, height = [int(v) for v in hlwm.call('monitor_rect').stdout.split(' ')][2:]
+
+    # go to left frame
+    mouse.move_to(width * 0.25, height * 0.5)
+
+    # go to right frame
+    mouse.move_to(width * 0.75, height * 0.5)
+    if click:
+        mouse.click('1')
+
+    # we expect the focus to go to the second frame
+    # if focus_follows_mouse is enabled or if we click explicitly
+    expected_focus = 1 if focus_follows_mouse or click else 0
+    assert layout_template.format(expected_focus) \
+        == hlwm.call('dump').stdout
