herbstluftwm/herbstluftwm

commit 73c64e78d0f52a0cec4f4f9598775839eddc2bfc

Only complete herbstclient options where appropriate
Herbstclient options must precede, if there is any, the first command to
herbstluftwm. Do not offer them as arguments to hlwm commands.

share/herbstclient.fish
@@ -1,19 +1,23 @@
 # fish completion for herbstclient
 
-function _get_herbstluftwm_completion
-    set -l tokens (commandline -op)
+function _first_hlwm_command
+    # Find first hlwm command in given tokens (e.g. commandline -o)
+    # Print token position if found; exit status 0 if found, else 1
 
-    # Delete "herbstclient"
-    set -e tokens[1]
-
-    # Filter herbstclient options
-    for token in $tokens
-        if string match -q -- "-*" $token
-            set -e tokens[1]
-        else
-            break
+    for i in (seq 2 (count $argv)) # start after 'herbstclient'
+        if not string match -q -- "-*" $argv[$i]
+            echo $i
+            return 0
         end
     end
+    return 1
+end
+
+function _get_herbstluftwm_completion
+    set tokens (commandline -o)
+    set first (_first_hlwm_command $tokens)
+    if not test -n $first; return; end # no command found, we are done
+    set tokens $tokens[$first..-1]
 
     set -l position (count $tokens)
 
@@ -25,14 +29,23 @@ function _get_herbstluftwm_completion
     herbstclient -q complete $position $tokens
 end
 
+function _complete_herbstclient
+    # do not complete herbstclient options after commands
+    # TODO: this could check if cursor sits in front of said command
+    complete -fc herbstclient -n 'not _first_hlwm_command (commandline -o)' $argv
+end
+
+# add completions for herbstclient options
+_complete_herbstclient -s n -l no-newline -d 'Do not print a newline if output does not end with a newline.'
+_complete_herbstclient -s 0 -l print0 -d 'Use the null character as delimiter between the output of hooks.'
+_complete_herbstclient -s l -l last-arg -d 'When using -i or -w, only print the last argument of the hook.'
+_complete_herbstclient -s i -l idle -d 'Wait for hooks instead of executing commands.'
+_complete_herbstclient -s w -l wait -d 'Same as --idle but exit after first --count hooks.'
+_complete_herbstclient -s c -l count -r -d 'Let --wait exit after COUNT hooks were received and printed.'
+_complete_herbstclient -s q -l quiet -d 'Do not print error messages if herbstclient cannot connect to the running herbstluftwm instance.'
+_complete_herbstclient -s v -l version -d 'Print the herbstclient version.'
+_complete_herbstclient -s h -l help -d 'Print the herbstclient usage with its command line options.'
+
+# defer all other completions to herbstluftwm
 complete -xc herbstclient -d "" -a '(_get_herbstluftwm_completion)'
 
-complete -fc herbstclient -s n -l no-newline -d 'Do not print a newline if output does not end with a newline.'
-complete -fc herbstclient -s 0 -l print0 -d 'Use the null character as delimiter between the output of hooks.'
-complete -fc herbstclient -s l -l last-arg -d 'When using -i or -w, only print the last argument of the hook.'
-complete -fc herbstclient -s i -l idle -d 'Wait for hooks instead of executing commands.'
-complete -fc herbstclient -s w -l wait -d 'Same as --idle but exit after first --count hooks.'
-complete -fc herbstclient -s c -l count -r -d 'Let --wait exit after COUNT hooks were received and printed.'
-complete -fc herbstclient -s q -l quiet -d 'Do not print error messages if herbstclient cannot connect to the running herbstluftwm instance.'
-complete -fc herbstclient -s v -l version -d 'Print the herbstclient version.'
-complete -fc herbstclient -s h -l help -d 'Print the herbstclient usage with its command line options.'

commit 250d42459b591301267b8f548eec3a04bdbbb89a

Update fish completions (#854)
* Only complete herbstclient options where appropriate

Herbstclient options must precede, if there is any, the first command to
herbstluftwm. Do not offer them as arguments to hlwm commands.

* Avoid spaces for partial completions

Fixes #797 with a workaround.

As fish does not seem to support partial completions, we detect partial
completions and eagerly fetch their subsequent completions.

This includes a switch to hlwm's complete_shell method:
 • Escape sequences are removed in COMMAND and ARGS.
 • Special characters will be escaped in the output.

* Only consider relevant part of command line

A regression introduced in 73c64e78d0 would lead to false completions
when hc is completed in a chain or pipe (e.g. echo hi; and hc <tab>)

* Correctly cut off preceding herbstclient options

Note: that test always passed, and was also misguided

* Do not complete hlwm commands where not expected

* Ignore commands after the cursor

Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

share/herbstclient.fish
@@ -2,21 +2,26 @@
 
 function _first_hlwm_command
     # Find first hlwm command in given tokens (e.g. commandline -o)
-    # Print token position if found; exit status 0 if found, else 1
+    # Print token position if found, else print no. tokens + 1
+    # Exit status 0 if found, 1 if not found, 2 if not allowed due to options
 
     for i in (seq 2 (count $argv)) # start after 'herbstclient'
+        if string match -qr -- '^(-v|-h|--help|-w|--wait|-i|--idle)$' $argv[$i]
+            return 2 # these options do not take commands
+        end
         if not string match -q -- "-*" $argv[$i]
             echo $i
             return 0
         end
     end
+    math (count $argv) + 1
     return 1
 end
 
 function _get_herbstluftwm_completion
-    set tokens (commandline -o)
+    set tokens (commandline -op)
     set first (_first_hlwm_command $tokens)
-    if not test -n $first; return; end # no command found, we are done
+    if test $status -eq 2; return; end # no hlwm completion desired
     set tokens $tokens[$first..-1]
 
     # TODO: we should derive the real position but it is tricky
@@ -44,8 +49,7 @@ end
 
 function _complete_herbstclient
     # do not complete herbstclient options after commands
-    # TODO: this could check if cursor sits in front of said command
-    complete -fc herbstclient -n 'not _first_hlwm_command (commandline -o)' $argv
+    complete -fc herbstclient -n 'not _first_hlwm_command (commandline -op --cut-at-cursor)' $argv
 end
 
 # add completions for herbstclient options