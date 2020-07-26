b04e14c2ee598174edb6dfb0ac1f68e5280b5057
Thorsten Wißmann <edu@thorsten-wissmann.de>
2020-07-26 15:06:42 +0200

    Fix cycle_layout for omitted layout names (#965)
    If a custom list of layouts is specified for cycle_layout and the
    current layout is not part of this list, then cycle_layout has to pick
    the first layout in the list according to the docs. The present commit
    re-establishes this behaviour and adds test cases for it.

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -813,3 +813,41 @@ def test_resize_clamp_argument_bigger(hlwm):
     hlwm.call(['load', layout.format('0.2')])
     hlwm.call('resize left 0.15')
     assert hlwm.call('dump').stdout == layout.format('0.1')
+
+
+def current_layout_name(hlwm):
+    proc = hlwm.call(['dump', '', '@'])
+    m = re.match(r'^\([^ ]* ([^:]*):.*\)$', proc.stdout)
+    return m.group(1)
+
+
+@pytest.mark.parametrize("delta", ['-1', '+1'])
+def test_cycle_layout_name_not_in_list(hlwm, delta):
+    hlwm.call('set_layout vertical')
+
+    # if the current name is not contained in the custom list
+    # of layouts, then the first entry in the custom list
+    # has to be picked
+    hlwm.call(f'cycle_layout {delta} max grid')
+
+    assert current_layout_name(hlwm) == 'max'
+
+
+@pytest.mark.parametrize("delta", ['-1', '+1'])
+def test_cycle_layout_name_in_the_list(hlwm, delta):
+    hlwm.call('set_layout vertical')
+    layouts = ['vertical', 'max', 'grid']
+    # here, 'layouts' does not contain 'horizontal'. By this, we
+    # implicitly verify that we never reach the 'horizontal' layout
+    # since the 'expected_layouts' list also does not contain 'horizontal'
+    cycle_layout_cmd = ['cycle_layout', delta] + layouts
+    # expected list of layouts after calling
+    # 'cycle_layout_cmd' multiple times:
+    if delta == '+1':
+        expected_layouts = layouts[1:] + layouts
+    elif delta == '-1':
+        expected_layouts = reversed(layouts + layouts)
+
+    for expected in expected_layouts:
+        hlwm.call(cycle_layout_cmd)
+        assert current_layout_name(hlwm) == expected
