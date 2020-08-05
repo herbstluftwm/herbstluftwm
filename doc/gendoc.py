6ec30cd7865b53a726de7ca9f8893b005ebebc19
Thorsten Wißmann <edu@thorsten-wissmann.de>
2020-08-05 23:39:36 +0200

    Improve tokenizer

--- doc/gendoc.py
+++ doc/gendoc.py
@@ -20,14 +20,15 @@ def extract_file_tokens(filepath):
     # in the following, it's important to use
     # non-capturing groups (?: .... )
     token_types = [
-        '#[a-zA-Z][^\n]*\n',  # preprocessor
+        '#[a-zA-Z](?:[^\n]|\\\n)*\n',  # preprocessor
         '//[^\n]*\n',  # single-line comment
         '/\*(?:[^\*]*|\**[^/*])*\*/',  # multiline comment
         '[a-zA-Z_][a-zA-Z0-9_]*',  # identifiers
+        '[0-9][0-9\.a-z]*', # numbers
         '\'(?:\\\'|[^\']*)\'',
         "\"(?:\\\"|[^\"]*)\"",
         "[-+<>/*]",  # operators
-        "[\(\),;:{}\?|~]",
+        r'[\(\),;:{}\[\]\?&|~]',
         '[\t ][\t ]*',
         '\n',
     ]
@@ -107,6 +108,7 @@ class TokenTree:
 
 
 class TokenStream:
+    """a stream of objects"""
     def __init__(self, tokens):
         self.tokens = tokens
         self.pos = 0
@@ -114,32 +116,82 @@ class TokenStream:
     def empty(self):
         return self.pos >= len(self.tokens)
 
-    def pop(self):
-        t =  self.tokens[self.pos]
+    def pop(self, error_message="expected another token"):
+        """pop a token from the stream. If the stream is empty
+        already, raise an exception instead
+        """
+        if self.pos >= len(self.tokens):
+            fullmsg = "Unexpected end of token stream: {}"
+            fullmsg = fullmsg.format(error_message)
+            raise Exception(fullmsg)
+        tok = self.tokens[self.pos]
+        print("tok {}".format(tok), file=sys.stderr)
         self.pos += 1
-        #print("yielding token '{}'".format(t))
-        return t
+        # print("yielding token '{}'".format(t))
+        return tok
 
     def undo_pop(self):
         """undo the last pop operation"""
+        print("tokundo", file=sys.stderr)
         self.pos -= 1
 
 
 def build_token_tree_list(token_stream):
     """return a list of TokenTree objects"""
+    matching = [
+        ('(', ')'),
+        ('{', '}'),
+        ('[', ']'),
+    ]
     while not token_stream.empty():
         t = token_stream.pop()
-        if t in [')', '}', ']']:
+        if t in [m[1] for m in matching]:  # closing scope
             token_stream.undo_pop()
             break
-        elif t in ['(', '{', '[']:
+        elif t in [m[0] for m in matching]:  # opening scope
             nested = list(build_token_tree_list(token_stream))
             closing = token_stream.pop()
-            #assert closing in [')', '}', ']']
+            if (t, closing) not in matching:
+                raise Exception("'{}' is closed by '{}'".format(t, closing))
             yield TokenTree.TokenGroup(t, nested, closing)
         else:
             yield TokenTree.LiterateToken(t)
 
+
+class ObjectInformation:
+    def __init__(self):
+        self.base_classes = {}  # mapping class names to base clases
+
+    def base_class(self, subclass, baseclass):
+        if subclass not in self.base_classes:
+            self.base_classes[subclass] = []
+        self.base_classes[subclass].append(baseclass)
+
+    def print(self):
+        for k, v in self.base_classes.items():
+            print("{} has the base classes: {}".format(k, ' '.join(v)))
+
+
+def extract_doc_info(tokentreenodes, objInfo):
+    """extract object information from a list of TokenTree objects
+    and save the data in the ObjectInformation object passed"""
+    pub_priv_prot = ['public', 'private', 'protected']
+    stream = TokenStream(tokentreenodes)
+    while not stream.empty():
+        token = stream.pop()
+        if token.literate == 'class':
+            classname = stream.pop("expecting class name after 'class'").literate
+            nexttoken = stream.pop("expecting something after 'class {}'"
+                                  .format(classname))
+            if nexttoken.literate == ':':
+                nexttoken = stream.pop("expecting base classes")
+                while nexttoken.literate not in [None, ';']:
+                    if nexttoken.literate not in pub_priv_prot:
+                        objInfo.base_class(classname, nexttoken.literate)
+                    nexttoken = stream.pop("expecting base classes")
+            if nexttoken.literate == ';':
+                # only a forward declaration
+                continue
 
 def main():
     parser = argparse.ArgumentParser(description='extract hlwm doc from the source code')
@@ -151,8 +203,12 @@ def main():
                         help='tokenize a particular file and then exit')
     parser.add_argument('--tokentree-single-file',
                         help='print the token tree of a particular file and then exit')
+    parser.add_argument('--objects', action='store_const', default=False, const=True,
+                        help='extract object information')
     args = parser.parse_args()
-    files = findfiles(args.sourcedir, re.compile(args.fileregex))
+
+    # only evaluated if needed:
+    files = lambda: findfiles(args.sourcedir, re.compile(args.fileregex))
 
     if args.tokenize_single_file is not None:
         tokens = extract_file_tokens(args.tokenize_single_file)
@@ -170,6 +226,16 @@ def main():
                 print("token: {}".format(stream.pop()), file=sys.stderr)
         TokenTree.PrettyPrintList(tt_list)
         return 0
+
+    if args.objects is not None:
+        objInfo = ObjectInformation()
+        for f in files():
+            print("parsing file {}".format(f))
+            toks = extract_file_tokens(f)
+            toktree = list(build_token_tree_list(TokenStream(list(toks))))
+            extract_doc_info(toktree, objInfo)
+        objInfo.print()
+
 
 EXITCODE = main()
 if EXITCODE is not None:
