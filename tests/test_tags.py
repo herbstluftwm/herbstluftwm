da3b0a917a8ac6fbf3c6f36bc32d3ed10de56db6
Thorsten Wißmann <edu@thorsten-wissmann.de>
2020-10-25 17:13:55 +0100

    Add test for tag_renamed for multiple tags
    This shows that a static variable in the lambda is not suitable

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -263,3 +263,17 @@ def test_urgent_count(hlwm, x11):
 
     # since one of them gets focused, 4 urgent clients remain
     assert int(hlwm.get_attr('tags.focus.urgent_count')) == 4
+
+
+def test_rename_different_tags(hlwm, hc_idle):
+    hlwm.call('add foo_old')
+    hlwm.call('add bar_old')
+    hc_idle.hooks()  # clear hooks
+
+    hlwm.call('rename foo_old foo_new')
+    hlwm.call('rename bar_old bar_new')
+
+    assert hc_idle.hooks() == [
+        ['tag_renamed', 'foo_old', 'foo_new'],
+        ['tag_renamed', 'bar_old', 'bar_new']
+    ]
