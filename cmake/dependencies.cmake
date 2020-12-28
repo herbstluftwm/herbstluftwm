611ae382f17d651c66a306e57f0ca5fb201e0fae
Thorsten Wißmann <edu@thorsten-wissmann.de>
2020-12-28 17:18:07 +0100

    Try plain pkg-config

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,6 +1,13 @@
 ## get project dependencies
 # Xlib
-find_package(X11 REQUIRED)
-find_package(Freetype2 QUIET)
+find_package(PkgConfig)
+
+pkg_check_modules(X11 x11)
+pkg_check_modules(XFT xft)
+pkg_check_modules(XRANDR xrandr)
+pkg_check_modules(XINERAMA xinerama)
+pkg_check_modules(XEXT xext)
+
+pkg_check_modules(FREETYPE freetype2)
 
 # vim: et:ts=4:sw=4
