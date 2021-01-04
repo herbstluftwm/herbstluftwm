12f06b2e5fb57da6a746a2e3484af0af4944edf0
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-01-04 11:48:30 +0100

    Roll back to pure cmake library detection

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,13 +1,7 @@
 ## get project dependencies
 # Xlib
-find_package(PkgConfig)
-
-pkg_check_modules(X11 x11)
-pkg_check_modules(XFT xft)
-pkg_check_modules(XRANDR xrandr)
-pkg_check_modules(XINERAMA xinerama)
-pkg_check_modules(XEXT xext)
-
-pkg_check_modules(FREETYPE freetype2)
+find_package(X11 REQUIRED)
+find_package(Freetype REQUIRED)
+find_package(Fontconfig REQUIRED)
 
 # vim: et:ts=4:sw=4
