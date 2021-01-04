9421d93e7473448325b4b2ad70e6848f0c1cdfbf
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-01-04 12:58:52 +0100

    Use pkg-config for library detection (#1108)
    Instead of using the FindX11.cmake module that comes with cmake, we now
    use pkg-config for the compiler and linker flags. This allows us to
    explicitly state which libraries we use. For FindX11.cmake it just
    tried to detect what's there, with little output. for pkg-config, cmake
    now clearly prints which libraries are found, and which are missing.
    
    This also adds the libraries freetype and xft that will be necessary
    for xft font rendering.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,5 +1,14 @@
 ## get project dependencies
 # Xlib
-find_package(X11 REQUIRED)
+find_package(PkgConfig)
+
+pkg_check_modules(X11 REQUIRED x11)
+pkg_check_modules(XRANDR REQUIRED xrandr)
+pkg_check_modules(XINERAMA xinerama)
+pkg_check_modules(XEXT REQUIRED xext)
+
+# for xft support:
+pkg_check_modules(XFT REQUIRED xft)
+pkg_check_modules(FREETYPE REQUIRED freetype2)
 
 # vim: et:ts=4:sw=4
