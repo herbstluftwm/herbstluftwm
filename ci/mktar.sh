7d6141f22c92464137f214c23daf28cce4090aa7
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-02-15 15:07:33 +0100

    Add mktar.sh

---
+++ ci/mktar.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+# create a source tarball including pre-built documentation
+builddir=${builddir:-build}
+version=${version:-$(git describe|sed 's,^v,,')}
+tarname=${tarname:-herbstluftwm-$version}
+
+::() {
+    echo -e "\e[1;33m:: \e[0;32m$*\e[0m" >&2
+    "$@"
+}
+
+# we first create a .tar file and then gzip it because otherwise
+# we can't add additional files to the tar
+
+# take the git source files
+:: git archive --prefix=$tarname/ -o ${tarname}.tar HEAD
+
+# add compiled documentation
+:: tar --transform="flags=r;s,${builddir}/,${tarname}/,"  \
+    --owner=0 --group=0 \
+    -uvf ${tarname}.tar ${builddir}/doc/*.{html,json,[1-9]}
+
+:: gzip ${tarname}.tar
+
+echo To sign the tarball, run: gpg --detach-sign ${tarname}.tar.gz >&2
