herbstluftwm/herbstluftwm

commit 2f337ab3d73431b02f31e7d3cfee3a60fe77cdb9

Only resend ConfigureNotify on geometry change (#1236)
This fixes an issue (#1235) which consisted of hlwm hanging because of a
loop between hlwm and the reaper application (reaper.fm).

Apparently, the issue is that `XA_WM_NORMAL_HINTS` is updated by the
reaper window, and this causes the tiling layout to be re-computed,
which results in a ConfigureNotify sent to the window. This event seems
to make reaper set `XA_WM_NORMAL_HINTS` again and so the whole process
starts again.

The solution is to only send the ConfigureNotify if the geometry really
changed. I don't know how to write a test case for it, but any test case
more or less should check that this still sends enough ConfigureNotify
events as requested by ICCCM (section 4.1.5.).

src/decoration.cpp
@@ -225,6 +225,7 @@ void Decoration::resize_outline(Rectangle outline, const DecorationScheme& schem
         // right when the window size changed.
         outline = scheme.inner_rect_to_outline(inner);
     }
+    bool windowGeometryChanged = inner != last_inner_rect;
     last_inner_rect = inner;
     inner.x -= outline.x;
     inner.y -= outline.y;
@@ -279,7 +280,12 @@ void Decoration::resize_outline(Rectangle outline, const DecorationScheme& schem
                       outline.x, outline.y, outline.width, outline.height);
     updateFrameExtends();
     if (!client_->dragged_ || settings_.update_dragged_clients()) {
-        client_->send_configure();
+        if (windowGeometryChanged) {
+            // only send the configure notify if the window geometry really changed.
+            // otherwise, sending this might trigger an endless loop between clients
+            // and hlwm.
+            client_->send_configure();
+        }
     }
     XSync(xcon.display(), False);
 }

commit 1306957a38342fa6cbe9cc65d618621b0d5c435a

Remember last reported geometry in client directly
Instead of deciding in Decoration when to send a configure, just
remember the last configure event in every client so we know directly
whether it is worth sending a new configure event or not.

This fixes the bug #1239 where moving a client with the mouse did not
send the configure event when the mouse dragging was over. (This cause
the menus in Qt applications to be positioned wrong.). The bug was
originally introduced by #1236, thanks to @tiosgz for reporting it!

For easier testing, this remembered window geometry is exposed via the
attribute system. To simplify writing tests for it, there is a new type
'Rectangle' in the python bindings.

python/herbstluftwm/types.py
@@ -1,0 +1,50 @@
+import re
+"""
+Module containing types used in the communication with herbstluftwm;
+primarily, types used in attributes.
+"""
+
+
+class Rectangle:
+    """
+    A rectangle on the screen, defined by its size and its distance to
+    the top left screen corner.
+    """
+    def __init__(self, x=0, y=0, width=0, height=0):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f'Rectangle({self.x}, {self.y}, {self.width}, {self.height})'
+
+    def __repr__(self):
+        return f'Rectangle({self.x}, {self.y}, {self.width}, {self.height})'
+
+    def __eq__(self, other):
+        return self.x == other.x \
+            and self.y == other.y \
+            and self.width == other.width \
+            and self.height == other.height
+
+    def to_user_str(self):
+        return "%dx%d%+d%+d" % (self.width, self.height, self.x, self.y)
+
+    @staticmethod
+    def from_user_str(string):
+        reg = '([0-9]+)x([0-9]+)([+-][0-9]+)([+-][0-9]+)'
+        m = re.match(reg, string)
+        if m is None:
+            raise Exception(f'"{string}" is not in format {reg}')
+        w = int(m.group(1))
+        h = int(m.group(2))
+        x = int(m.group(3))
+        y = int(m.group(4))
+        return Rectangle(x, y, w, h)
+
+    def adjusted(self, dx=0, dy=0, dw=0, dh=0):
+        """return a new rectangle whose components
+        are adjusted by the provided deltas.
+        """
+        return Rectangle(self.x + dx, self.y + dy, self.width + dw, self.height + dh)

src/client.cpp
@@ -56,6 +56,7 @@ Client::Client(Window window, bool visible_already, ClientManager& cm)
     , sizehints_tiling_(this, "sizehints_tiling", false)
     , window_class_(this, "class", &Client::getWindowClass)
     , window_instance_(this, "instance", &Client::getWindowInstance)
+    , geometry_reported_(this, "geometry_reported", {})
     , manager(cm)
     , theme(*cm.theme)
     , settings(*cm.settings)
@@ -135,6 +136,11 @@ Client::Client(Window window, bool visible_already, ClientManager& cm)
                              "should be respected in tiling mode");
     sizehints_floating_.setDoc("if sizehints for this client should "
                                "be respected in floating mode");
+    geometry_reported_.setDoc(
+                "the geometry the client thinks it has. "
+                "This is the last window geometry that "
+                "was reported to the client application."
+     );
 }
 
 void Client::init_from_X() {
@@ -428,6 +434,13 @@ void Client::updatesizehints() {
 
 void Client::send_configure() {
     auto last_inner_rect = dec->last_inner();
+    if (geometry_reported_ == last_inner_rect) {
+        // only send the configure notify if the window geometry really changed.
+        // otherwise, sending this might trigger an endless loop between clients
+        // and hlwm.
+        return;
+    }
+    geometry_reported_ = last_inner_rect;
     XConfigureEvent ce;
     ce.type = ConfigureNotify;
     ce.display = X_.display();

src/client.h
@@ -78,6 +78,7 @@ public:
     Attribute_<bool> sizehints_tiling_;  // respect size hints regarding this client in tiling mode
     DynAttribute_<std::string> window_class_;
     DynAttribute_<std::string> window_instance_;
+    Attribute_<Rectangle> geometry_reported_;
 
 public:
     void init_from_X();

src/decoration.cpp
@@ -225,7 +225,6 @@ void Decoration::resize_outline(Rectangle outline, const DecorationScheme& schem
         // right when the window size changed.
         outline = scheme.inner_rect_to_outline(inner);
     }
-    bool windowGeometryChanged = inner != last_inner_rect;
     last_inner_rect = inner;
     inner.x -= outline.x;
     inner.y -= outline.y;
@@ -280,12 +279,7 @@ void Decoration::resize_outline(Rectangle outline, const DecorationScheme& schem
                       outline.x, outline.y, outline.width, outline.height);
     updateFrameExtends();
     if (!client_->dragged_ || settings_.update_dragged_clients()) {
-        if (windowGeometryChanged) {
-            // only send the configure notify if the window geometry really changed.
-            // otherwise, sending this might trigger an endless loop between clients
-            // and hlwm.
-            client_->send_configure();
-        }
+        client_->send_configure();
     }
     XSync(xcon.display(), False);
 }

tests/test_mousebind.py
@@ -1,6 +1,7 @@
 import pytest
 import re
 import math
+from herbstluftwm.types import Rectangle
 
 # Note: For unknown reasons, mouse buttons 4 and 5 (scroll wheel) do not work
 # in Xvfb when running tests in the CI. Therefore, we maintain two lists of
@@ -119,6 +120,25 @@ def test_drag_move(hlwm, x11, mouse, repeat):
     assert x11.get_absolute_top_left(client) == (x + 12, y + 15)
 
 
+@pytest.mark.parametrize('update_dragged', [True, False])
+def test_drag_move_sends_configure(hlwm, x11, mouse, update_dragged):
+    hlwm.attr.tags.focus.floating = 'on'
+    hlwm.attr.settings.update_dragged_clients = hlwm.bool(update_dragged)
+    client, winid = x11.create_client()
+    x, y = x11.get_absolute_top_left(client)
+    before = Rectangle.from_user_str(hlwm.attr.clients[winid].geometry_reported())
+    assert (x, y) == (before.x, before.y)
+    mouse.move_into(winid, wait=True)
+
+    hlwm.call(['drag', winid, 'move'])
+    mouse.move_relative(12, 15)
+    mouse.click('1')  # stop dragging
+    hlwm.call('true')  # sync
+
+    after = Rectangle.from_user_str(hlwm.attr.clients[winid].geometry_reported())
+    assert before.adjusted(dx=12, dy=15) == after
+
+
 def test_drag_no_frame_splits(hlwm):
     winid, _ = hlwm.create_client()
 
@@ -183,12 +203,12 @@ def test_drag_resize_tiled_client(hlwm, mouse):
 
 @pytest.mark.parametrize('live_update', [True, False])
 def test_drag_resize_floating_client(hlwm, x11, mouse, live_update):
-    hlwm.call(['set', 'update_dragged_clients', hlwm.bool(live_update)])
+    hlwm.attr.settings.update_dragged_clients = hlwm.bool(live_update)
 
     client, winid = x11.create_client(geometry=(50, 50, 300, 200))
     hlwm.call(f'set_attr clients.{winid}.floating true')
-    geom_before = client.get_geometry()
-    x_before, y_before = x11.get_absolute_top_left(client)
+    geom_before = Rectangle.from_user_str(hlwm.attr.clients[winid].geometry_reported())
+    assert geom_before == x11.get_absolute_geometry(client)  # duck-typing
     assert (geom_before.width, geom_before.height) == (300, 200)
     # move cursor to the top left corner, so we change the
     # window position and the size (and the bottom right corner is fixed)
@@ -204,7 +224,7 @@ def test_drag_resize_floating_client(hlwm, x11, mouse, live_update):
     x11.display.sync()
     geom_after = client.get_geometry()
     x_after, y_after = x11.get_absolute_top_left(client)
-    assert (x_after, y_after) == (x_before + 100, y_before + 120)
+    assert (x_after, y_after) == (geom_before.x + 100, geom_before.y + 120)
     expected_size = (geom_before.width, geom_before.height)
     if live_update:
         expected_size = final_size
@@ -214,6 +234,8 @@ def test_drag_resize_floating_client(hlwm, x11, mouse, live_update):
     mouse.click('1', wait=True)
     geom_after = client.get_geometry()
     assert (geom_after.width, geom_after.height) == final_size
+    assert Rectangle.from_user_str(hlwm.attr.clients[winid].geometry_reported()) \
+        == geom_before.adjusted(dx=100, dy=120, dw=-100, dh=-120)
 
 
 def test_drag_zoom_floating_client(hlwm, x11, mouse):