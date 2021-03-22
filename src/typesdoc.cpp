ac87d90a16b2b12009ce1326265f22c08c7e336c
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-03-22 09:21:17 +0100

    Fix types doc on fonts (#1242)
    There are two issues in the documentation on the font type that are
    fixed by adjusting the documentation that comes out of the json file:
    
      - nested bullet items are now indented correctly.
      - everything within single-quotes is now treated as-is, so no
        special characters can be interpreted by asciidoc.
    
    The diff on the man page is the following. Note how the indentation and
    the asterisks of the example fonts are correct now.
    
    ```
                •   Fontconfig description. This supports antialiased
                    fonts, for example:
    
    -       •   Dejavu Sans:pixelsize=12
    +               •   Dejavu Sans:pixelsize=12
    
    -       •   Bitstream Vera Sans:size=12:bold
    +               •   Bitstream Vera Sans:size=12:bold
    
                •   X logical font description (XLFD), as provided by the
                    xfontsel tool. No antialiasing is supported here, but
                    this is usually superior for bitmap fonts. For
                    example:
    
    -       •   --fixed-medium-r---13------- for a standard bitmap font
    -           available on most systems. For attributes and children,
    -           see types.bool
    +               •   -*-fixed-medium-r-*-*-13-*-*-*-*-*-*-* for a
    +                   standard bitmap font available on most systems.
    +                   For attributes and children, see types.bool
    
            •   int: Type representing signed integers. For attributes
                and children, see types.bool
    ```

--- src/typesdoc.cpp
+++ src/typesdoc.cpp
@@ -77,7 +77,8 @@ TypesDoc::TypesDoc()
          "- X logical font description (XLFD), as provided by the\n"
          "  xfontsel tool. No antialiasing is supported here, but this\n"
          "  is usually superior for bitmap fonts. For example:\n"
-         "  * '-*-fixed-medium-r-*-*-13-*-*-*-*-*-*-*'\n"
+         "  * '-*-fixed-medium-r-*-*-13-*-*-*-*-*-*-*' for a standard \n"
+         "    bitmap font available on most systems\n"
     );
 
     int_.init(Type::INT);
