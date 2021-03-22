7edc3d15c0fb1ca895e93ba8a507a3f420002873
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-03-22 20:21:17 +0100

    Add types doc for regex and window id (#1243)
    This documents the syntax of the remaining two attribute types.

--- src/typesdoc.cpp
+++ src/typesdoc.cpp
@@ -35,10 +35,10 @@ TypesDoc::TypesDoc()
     , int_(*this, "int")
     , names_(*this, "names")
     , rectangle_(*this, "rectangle")
-    // , regex_(*this, "regex") // TODO
+    , regex_(*this, "regex")
     , string_(*this, "string")
     , uint_(*this, "uint")
-    // , windowid_(*this, "windowid") // TODO
+    , windowid_(*this, "windowid")
 {
     setDoc(
         "This lists the types that are used for attributes and command arguments.");
@@ -105,11 +105,33 @@ TypesDoc::TypesDoc()
         "800x600+800+0 or 400x200-10+30"
     );
 
+    regex_.init(Type::REGEX);
+    regex_.setDoc(
+        "A POSIX extended regular expression. Note that when passing "
+        "a regex on the command line, additional quoting can be "
+        "necessary. "
+        "For explanations and examples, see section 9.4.6 "
+        "of the documentation: "
+        "https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap09.html#tag_09_04_06"
+    );
+
     string_.init(Type::STRING);
     string_.setDoc("Type representing normal text.");
 
     uint_.init(Type::ULONG);
     uint_.setDoc("Type representing unsigned (i.e. non-negative) integers");
+
+    windowid_.init(Type::WINDOW);
+    windowid_.setDoc(
+        "The window id is the number of a window. This can be a managed window "
+        "(i.e. client) or an unmanaged window (e.g. a panel, a menu, "
+        "or a desktop window).\n"
+        "The default format is 0xHEX where HEX is a hexadecimal number "
+        "(digits 0-9 and a-f) but it can also be specified in the decimal "
+        "system (base 10), or as an octal number (with prefix 0 and base 8).\n"
+        "When a window id is printed, it is always printed in the 0xHEX format "
+        "and without any leading zeroes."
+    );
 }
 
 TypesDoc::~TypesDoc()
