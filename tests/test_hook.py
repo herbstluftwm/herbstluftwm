608d1d1882c5ad80ba21d74fe2d8a7f2bb75a543
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-03-27 15:19:51 +0100

    Only emit Attribute_::changed() on real change (#1248)
    The signal changed() present for all attributes was always emitted when
    the attribute was written, even if the same value is assigned to it.
    This issue only existed for direct assignments (operator=) in the source
    code. For re-assignments via the attribute commands, the signal was only
    emitted if the value really changed.
    
    Changing the semantics of the signal has of course much impact, but when
    using it so far, I always thought it has the 'only on change' semantics
    (just as the name suggests). Moreover, it makes sense because we can
    then use signals in the X11-modules to automatically forward information
    to X11 whenever attributes (properly) change.
    
    The present change uncovered a bug: in the smart placement, the variable
    Decoration::last_scheme was used implicitly, even though it was only
    initialized by accident (one of the signals called applyLayout()). Now,
    Decoration::inner_to_outer() picks a reasonable scheme if last_scheme is
    not yet initialized. The test case for this is introduced in #1247.

--- tests/test_hook.py
+++ tests/test_hook.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_emit_hook(hlwm, hc_idle):
     hlwm.call('emit_hook my_hook')
     assert hc_idle.hooks() == [['my_hook']]
@@ -17,8 +20,19 @@ def test_hc_idle_fixture_collects(hlwm, hc_idle):
 
 
 def test_fullscreen_change_emits_hook(hlwm, hc_idle):
+    hlwm.call('rule fullscreen=on')
     winid, _ = hlwm.create_client()
+    assert ['fullscreen', 'on', str(winid)] in hc_idle.hooks()
+
+    hlwm.call('set_attr clients.focus.fullscreen off')
     assert ['fullscreen', 'off', str(winid)] in hc_idle.hooks()
 
-    hlwm.call('set_attr clients.focus.fullscreen on')
-    assert ['fullscreen', 'on', str(winid)] in hc_idle.hooks()
+
+@pytest.mark.parametrize("change", [True, False])
+def test_no_fullscreen_hook_if_nothing_changes(hlwm, hc_idle, change):
+    if change:
+        hlwm.call('rule fullscreen=on')
+    winid, _ = hlwm.create_client()
+
+    # a fullscreen hook is fired if and only if a proper change happens
+    assert ('fullscreen' in [hook[0] for hook in hc_idle.hooks()]) == change
