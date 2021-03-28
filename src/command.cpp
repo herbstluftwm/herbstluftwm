4e1f05b506f61a0f191695a8210decfdfcdb145c
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-03-28 10:35:00 +0200

    Migrate 'add_monitor' to ArgParse (#1254)
    This migrates the 'add_monitor' command to ArgParse, and now also
    verifies that the given rectangle has at least the minimum window size.

--- src/command.cpp
+++ src/command.cpp
@@ -68,7 +68,6 @@ struct {
     { "rename",         3,  no_completion },
     { "move",           2,  no_completion },
     { "move_index",     3,  no_completion },
-    { "add_monitor",    7,  no_completion },
     { "monitor_rect",   3,  no_completion },
     { "layout",         3,  no_completion },
     { "dump",           3,  no_completion },
@@ -97,7 +96,6 @@ struct {
     const char** list;
 } g_completions[] = {
     /* name , relation, index,  completion method                   */
-    { "add_monitor",    EQ, 2,  complete_against_tags, 0 },
     { "close",          EQ, 1,  complete_against_winids, 0 },
     { "cycle",          EQ, 1,  nullptr, completion_pm_one },
     { "cycle_monitor",  EQ, 1,  nullptr, completion_pm_one },
