8df077b163acbef1112ecf0cd8369b2ec7cdf14a
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-04-21 10:59:34 +0200

    Write 'command not found' to the error channel
    This is yet another error message that belongs to the new error channel.

--- tests/test_herbstluftwm.py
+++ tests/test_herbstluftwm.py
@@ -147,3 +147,12 @@ def test_herbstluftwm_version_flags():
         assert proc.stderr == ''
         # look for some flag on stdout:
         assert re.search('herbstluftwm', proc.stdout)
+
+
+def test_command_not_found(hlwm):
+    command = 'nonexistentcommand'
+    message = f'Command "{command}" not found'
+    hlwm.call_xfail(command).expect_stderr(message)
+    hlwm.call_xfail(f'{command} argument').expect_stderr(message)
+    hlwm.call_xfail(f'chain , echo foo , {command} argument , anothercmd') \
+        .expect_stderr(message)
