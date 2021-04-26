4b578e71ee61edafcbc28f48fc602cef8b86748a
Simon Kuhnle <simon@blarzwurst.de>
2021-04-26 08:40:22 +0200

    utils: Remove unused method (#1293)
    posix_sh_compress_inplace seems unused, posix_sh_escape on the other
    hand is not.

--- src/utils.cpp
+++ src/utils.cpp
@@ -258,20 +258,3 @@ string posix_sh_escape(const string& source) {
     }
     return target;
 }
-
-void posix_sh_compress_inplace(char* str) {
-    int offset = 0;
-    for (int i = 0; true ; i++) {
-        if (str[i] == '\\' && str[i + 1] ) {
-            str[i + offset] = str[i + 1];
-            i++;
-            offset --;
-        } else {
-            str[i + offset] = str[i];
-        }
-        if (!str[i]) {
-            break;
-        }
-    }
-}
-
