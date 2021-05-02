1a4157735a0f0e83884f8aa076fd7fc3f228ce95
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-05-02 22:04:52 +0200

    Fix exec error message in spawn command (#1304)
    Print the correct command name if exec fails (instead of the memory
    address). The new test case verifies that this does not happen again.
    
    This fixes #1302.

--- tests/test_spawn.py
+++ tests/test_spawn.py
@@ -5,3 +5,13 @@ def test_spawn(hlwm, hlwm_process):
         assert proc.returncode == 0
         assert not proc.stderr
         assert not proc.stdout
+
+
+def test_spawn_command_not_exist(hlwm, hlwm_process):
+    cmdname = 'this_command_does_not_exist'
+    with hlwm_process.wait_stderr_match(f'execvp "{cmdname}" failed'):
+        cmd = ['spawn', cmdname]
+        proc = hlwm.unchecked_call(cmd, read_hlwm_output=False)
+        assert proc.returncode == 0
+        assert not proc.stderr
+        assert not proc.stdout
