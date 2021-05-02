3e83b3488ca9ccf06230853fcfa4df172571ac6e
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-05-02 15:43:45 +0200

    Another fallback

--- ipc-client/ipc-client.c
+++ ipc-client/ipc-client.c
@@ -122,7 +122,10 @@ bool hc_send_command(HCConnection* con, int argc, char* argv[],
     // set arguments
     XTextProperty text_prop;
     if (Success != Xutf8TextListToTextProperty(con->display, argv, argc, XUTF8StringStyle, &text_prop)) {
-        fprintf(stderr, "Error: could not create Utf-8 TextProperty\n");
+        if (Success != XmbTextListToTextProperty(con->display, argv, argc, XStdICCTextStyle, &text_prop)) {
+            fprintf(stderr, "Error: can not create TextProperty\n");
+            return false;
+        }
     }
     XSetTextProperty(con->display, con->client_window, &text_prop, con->atom_args);
     XFree(text_prop.value);
