a81cf9b89e7d22b9e5822798efe0c4639781c015
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-07-28 21:23:36 +0200

    Reorder includes

--- src/xmainloop.cpp
+++ src/xmainloop.cpp
@@ -1,9 +1,9 @@
 #include "xmainloop.h"
 
-#include <X11/cursorfont.h>
 #include <X11/X.h>
 #include <X11/Xatom.h>
 #include <X11/Xlib.h>
+#include <X11/cursorfont.h>
 #include <sys/wait.h>
 #include <iostream>
 #include <memory>
