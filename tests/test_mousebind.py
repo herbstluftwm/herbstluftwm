herbstluftwm/herbstluftwm

commit 5b4c0cf975f00e6dd12f36884b97944ad9ec4c50

Handle mouse clicks in correct order (#1404)
This implements the following fixes:

  1. an unfocused window can now be mouse-resized without getting
     focused.
  2. mouse-resizing a tiled window with tabs now works also at the
     decoration edge at unfocused tabs (i.e. if the second tab is
     focused, then one can resize the window by dragging the top left
     corner and without activating the first tab).

There are now test cases that verify both these fixes.

src/xmainloop.cpp
@@ -242,25 +242,31 @@ void XMainLoop::buttonpress(XButtonEvent* be) {
             client = Decoration::toClient(be->window);
         }
         if (client) {
-            Client* tabClient = {};
-            if (be->window == client->dec->decorationWindow()
-                && be->button == Button1)
-            {
-                auto maybeClick = client->dec->positionHasButton({be->x, be->y});
-                if (maybeClick.has_value()) {
-                    tabClient = maybeClick.value().tabClient_;
-                }
+            ResizeAction resize = {};
+            bool decorationClicked = be->window == client->decorationWindow();
+            if (decorationClicked) {
+                resize = client->dec->positionTriggersResize({be->x, be->y});
             }
-            bool raise = root_->settings->raise_on_click();
-            if (tabClient) {
-                focus_client(tabClient, false, true, raise);
+            if (resize) {
+                mm->mouse_initiate_resize(client, resize);
             } else {
-                focus_client(client, false, true, raise);
-                if (be->window == client->decorationWindow()) {
-                    ResizeAction resize = client->dec->positionTriggersResize({be->x, be->y});
-                    if (resize) {
-                        mm->mouse_initiate_resize(client, resize);
-                    } else {
+                // if no resize action is triggered, check whether
+                // a tab was clicked:
+                Client* tabClient = {};
+                if (be->window == client->dec->decorationWindow()
+                    && be->button == Button1)
+                {
+                    auto maybeClick = client->dec->positionHasButton({be->x, be->y});
+                    if (maybeClick.has_value()) {
+                        tabClient = maybeClick.value().tabClient_;
+                    }
+                }
+                bool raise = root_->settings->raise_on_click();
+                if (tabClient) {
+                    focus_client(tabClient, false, true, raise);
+                } else {
+                    focus_client(client, false, true, raise);
+                    if (decorationClicked) {
                         mm->mouse_initiate_move(client, {});
                     }
                 }

tests/test_mousebind.py
@@ -243,6 +243,30 @@ def test_drag_resize_tiled_client_in_two_directions(hlwm, mouse, dir1, dir2):
     assert math.isclose(corner_expected.y, corner_after.y, abs_tol=0.01)
 
 
+def test_client_resize_does_not_switch_tabs(hlwm, mouse):
+    bw = 5
+    hlwm.attr.theme.border_width = bw
+    hlwm.attr.theme.title_height = 20
+    winids = hlwm.create_clients(2)
+    winids_str = ' '.join(winids)
+    layout = f'(split horizontal:0.5:0 (clients max:0 {winids_str}) (clients vertical:0))'
+    hlwm.call(['load', layout])
+
+    client_geo = hlwm.attr.clients[winids[0]].decoration_geometry()
+    # clicking the top right corner does not switch the tabs:
+    mouse.move_to(client_geo.x + client_geo.width - bw // 2, client_geo.y + bw // 2)
+    mouse.mouse_press('1')
+
+    assert hlwm.attr.clients.dragged.winid() == winids[0]
+    assert hlwm.attr.clients.focus.winid() == winids[0]
+
+    # double check, that the tab was indeed just nearby:
+    mouse.mouse_release('1')
+    mouse.move_relative(-bw, bw)
+    mouse.click('1')
+    assert hlwm.attr.clients.focus.winid() == winids[1]
+
+
 @pytest.mark.parametrize('live_update', [True, False])
 def test_drag_resize_floating_client(hlwm, x11, mouse, live_update):
     hlwm.attr.settings.update_dragged_clients = hlwm.bool(live_update)
@@ -332,6 +356,50 @@ def test_move_client_via_decoration(hlwm, x11, mouse, repeat):
         == (size_after.width, size_after.height)
     # but the location
     assert expected_position == x11.get_absolute_top_left(client)
+
+
+@pytest.mark.parametrize('floating', [True, False])
+def test_resize_unfocused_client(hlwm, mouse, floating):
+    hlwm.attr.theme.border_width = 5
+    winid, _ = hlwm.create_client()
+    focus, _ = hlwm.create_client()
+    # place clients side by side, with the focus on the left
+    layout = f'(split horizontal:0.5:0 (clients vertical:0 {focus}) (clients vertical:0 {winid}))'
+    hlwm.call(['load', layout])
+    if floating:
+        # steal the tiling layout to the floating geometries:
+        hlwm.attr.clients[winid].floating_geometry = hlwm.attr.clients[winid].content_geometry()
+        hlwm.attr.clients[focus].floating_geometry = hlwm.attr.clients[focus].content_geometry()
+        hlwm.attr.tags.focus.floating = True
+        hlwm.attr.clients[winid].sizehints_floating = False
+        hlwm.attr.settings.snap_distance = 0
+
+    assert hlwm.attr.clients.focus.winid() == focus
+
+    geo = hlwm.attr.clients[winid].decoration_geometry()
+    # move the cursor into the left border of the right hand client:
+    mouse.move_to(geo.x + 2, geo.y + geo.height // 2)
+    mouse.mouse_press('1')
+    assert hlwm.attr.clients.focus.winid() == focus
+    assert hlwm.attr.clients.dragged.winid() == winid
+
+    dx = 17
+    mouse.move_relative(-dx, -20)
+    mouse.mouse_release('1')
+    assert 'dragged' not in hlwm.list_children('clients')
+    new_geo = hlwm.attr.clients[winid].decoration_geometry()
+    # the window grew 13 pixels to the left:
+    assert new_geo.x == geo.x - dx
+    assert new_geo.width == geo.width + dx
+    assert hlwm.attr.clients.focus.winid() == focus, \
+        "the focus didn't change"
+    fraction_attr = hlwm.attr.tags.focus.tiling.root.fraction
+    if floating:
+        assert fraction_attr() == '0.5', \
+            "the tiling shouldn't change in floating resize"
+    else:
+        assert fraction_attr() != '0.5', \
+            "the tiling layout is updated"
 
 
 # we had a race condition here, so increase the likelyhood

tests/conftest.py
@@ -1077,7 +1077,7 @@ def mouse(hlwm_process, hlwm):
                 hlwm.call('true')
 
         def move_relative(self, delta_x, delta_y, wait=True):
-            self.call_cmd(f'xdotool mousemove_relative --sync {delta_x} {delta_y}', shell=True)
+            self.call_cmd(f'xdotool mousemove_relative --sync -- {delta_x} {delta_y}', shell=True)
             if wait:
                 # wait until all the mouse move events that were put in the
                 # queue by the above xdotool invokation are fully processed.

tests/test_basic_events.py
@@ -85,9 +85,11 @@ def test_focus_frame_by_mouse(hlwm, mouse, click, focus_follows_mouse):
 @pytest.mark.parametrize("click", [True, False])
 @pytest.mark.parametrize("focus_follows_mouse", [True, False])
 def test_focus_client_by_decoration(hlwm, mouse, x11, click, focus_follows_mouse):
-    hlwm.call('attr theme.border_width 50')
-    hlwm.call('attr theme.active.color red')
-    hlwm.call('attr theme.normal.color blue')
+    hlwm.attr.theme.border_width = 1
+    for side in ['top', 'bottom', 'left', 'right']:
+        hlwm.attr.theme[f'padding_{side}'] = 49
+    hlwm.attr.theme.active.color = 'red'
+    hlwm.attr.theme.normal.color = 'blue'
     hlwm.call('set_layout vertical')
     hlwm.call(['set', 'focus_follows_mouse', hlwm.bool(focus_follows_mouse)])
     winids = hlwm.create_clients(2)

commit 81074c3bac16cb29b14ce02b3afa41dcb5988146

Check early on click if client resize is possible
When clicking a client's border, check it early whether the client can
be resized at all. If it can not be resized, then focus the client
instead, because this is what the (normal) cursor shape indicates.

This closes #1407, a bug introduced by PR #1404.

src/xmainloop.cpp
@@ -246,6 +246,9 @@ void XMainLoop::buttonpress(XButtonEvent* be) {
             bool decorationClicked = be->window == client->decorationWindow();
             if (decorationClicked) {
                 resize = client->dec->positionTriggersResize({be->x, be->y});
+                if (resize) {
+                    resize = resize * client->possibleResizeActions();
+                }
             }
             if (resize) {
                 mm->mouse_initiate_resize(client, resize);

tests/test_mousebind.py
@@ -402,6 +402,38 @@ def test_resize_unfocused_client(hlwm, mouse, floating):
             "the tiling layout is updated"
 
 
+@pytest.mark.parametrize('resize_possible', [True, False])
+def test_border_click_either_focuses_or_resizez(hlwm, mouse, resize_possible):
+    """
+    when clicking on the outer decoration of a window, then this should either
+    trigger the resize or focus the client (if resizing is not possible).
+    """
+    hlwm.attr.theme.border_width = 5
+    hlwm.attr.settings.focus_follows_mouse = False
+    target, _ = hlwm.create_client()
+    focus, _ = hlwm.create_client()
+    if resize_possible:
+        # place clients below each other, with focus on the top
+        split_align = 'vertical'
+    else:
+        # place clients side by side, with the focus on the left
+        split_align = 'horizontal'
+    layout = f'(split {split_align}:0.5:0 (clients vertical:0 {focus}) (clients vertical:0 {target}))'
+    hlwm.call(['load', layout])
+    assert hlwm.attr.clients.focus.winid() == focus
+
+    target_geo = hlwm.attr.clients[target].decoration_geometry()
+    # click into the top of the decoration of the unfocused window.
+    click_point = target_geo.topleft() + Point(target_geo.width // 2, 3)
+    mouse.move_to(click_point.x, click_point.y)
+    mouse.mouse_press('1')
+
+    # this either starts dragging or focuses the 'target' window
+    focus_change = not resize_possible
+    assert ('dragged' in hlwm.list_children('clients')) == resize_possible
+    assert (hlwm.attr.clients.focus.winid() == target) == focus_change
+
+
 # we had a race condition here, so increase the likelyhood
 # that we really fixed it:
 @pytest.mark.parametrize('repeat', list(range(0, 100)))