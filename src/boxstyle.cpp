31ebbe26d6df783c9e9b03fb5773ba06abbe5813
Thorsten Wißmann <edu@thorsten-wissmann.de>
2022-07-10 19:02:14 +0200

    Add src files

---
+++ src/boxstyle.cpp
@@ -0,0 +1,384 @@
+#include "boxstyle.h"
+
+#include "globals.h"
+
+using std::function;
+using std::pair;
+using std::string;
+using std::stringstream;
+using std::to_string;
+using std::vector;
+
+const char BoxStyle::auto_[] = "auto";
+const char BoxStyle::solid[] = "solid";
+const char BoxStyle::transparent[] = "transparent";
+
+std::map<string, CssValueParser> CssValueParser::propName2Parser_;
+
+
+template<> CssLen Converter<CssLen>::parse(const string& source)
+{
+    if (source.size() < 2 || source.substr(source.size() - 2) != "px") {
+        throw std::invalid_argument("length must be of the format \"<n>px\", e.g. 4px");
+    }
+    int pxInt = Converter<int>::parse(source.substr(0, source.size() - 2));
+    CssLen len = pxInt;
+    return len;
+}
+
+template<> string Converter<CssLen>::str(CssLen payload)
+{
+    return Converter<int>::str(payload.inPixels_) + "px";
+}
+
+template<> void Converter<CssLen>::complete(Completion&, CssLen const*)
+{
+}
+
+
+template<>
+Finite<CssDisplay>::ValueList Finite<CssDisplay>::values = ValueListPlain {
+    { CssDisplay::none, "none" },
+    { CssDisplay::flex, "flex" },
+};
+
+
+/**
+ * @brief just an intermediate class to hide
+ * templated constructors from the .h file.
+ * In a FixedLenParser, only one parser function
+ * is set, whereas in CssValueParser, multiple parsers
+ * might be set.
+ */
+class FixedLenParser : public CssValueParser {
+public:
+    // on old compilers, we can't use the protected 'Str'
+    // so just define the shortcut a second time:
+    using StrRef = const string&;
+
+    template<typename A>
+    FixedLenParser(A BoxStyle::*member)
+    {
+        parser1_ = [member](const string& arg) {
+            A argTyped = Converter<A>::parse(arg);
+            return [member,argTyped](BoxStyle& style) -> void{
+                style.*member = argTyped;
+            };
+        };
+        getter_ = [member](const BoxStyle& style) -> string {
+            return Converter<A>::str(style.*member);
+        };
+        valuesMatch_ = [member](const BoxStyle& s1, const BoxStyle& s2) -> bool {
+            return s1.*member == s2.*member;
+        };
+    }
+
+    template<typename A1>
+    FixedLenParser(std::initializer_list<A1 BoxStyle::*> m1l)
+    {
+        vector<A1 BoxStyle::*> m1s = m1l;
+        parser1_ = [m1s](StrRef arg1) {
+            A1 arg1typed = Converter<A1>::parse(arg1);
+            return [m1s,arg1typed](BoxStyle& style) -> void{
+                for (auto m1 : m1s) {
+                    style.*m1 = arg1typed;
+                }
+            };
+        };
+    }
+
+    template<typename A1, typename A2>
+    FixedLenParser(std::initializer_list<A1 BoxStyle::*> m1l,
+                   std::initializer_list<A2 BoxStyle::*> m2l)
+    {
+        vector<A1 BoxStyle::*> m1s = m1l;
+        vector<A2 BoxStyle::*> m2s = m2l;
+        parser2_ = [m1s,m2s](StrRef arg1, StrRef arg2) {
+            A1 arg1typed = Converter<A1>::parse(arg1);
+            A2 arg2typed = Converter<A2>::parse(arg2);
+            return [m1s,m2s,arg1typed,arg2typed](BoxStyle& style) -> void{
+                for (auto m1 : m1s) {
+                    style.*m1 = arg1typed;
+                }
+                for (auto m2 : m2s) {
+                    style.*m2 = arg2typed;
+                }
+            };
+        };
+    }
+
+    template<typename A1, typename A2, typename A3>
+    FixedLenParser(std::initializer_list<A1 BoxStyle::*> m1l,
+                   std::initializer_list<A2 BoxStyle::*> m2l,
+                   std::initializer_list<A3 BoxStyle::*> m3l)
+    {
+        vector<A1 BoxStyle::*> m1s = m1l;
+        vector<A2 BoxStyle::*> m2s = m2l;
+        vector<A3 BoxStyle::*> m3s = m3l;
+        parser3_ = [m1s,m2s,m3s](StrRef arg1, StrRef arg2, StrRef arg3) {
+            A1 arg1typed = Converter<A1>::parse(arg1);
+            A2 arg2typed = Converter<A2>::parse(arg2);
+            A3 arg3typed = Converter<A3>::parse(arg3);
+            return [m1s,m2s,m3s,arg1typed,arg2typed,arg3typed](BoxStyle& style) -> void{
+                for (auto m1 : m1s) {
+                    style.*m1 = arg1typed;
+                }
+                for (auto m2 : m2s) {
+                    style.*m2 = arg2typed;
+                }
+                for (auto m3 : m3s) {
+                    style.*m3 = arg3typed;
+                }
+            };
+        };
+    }
+
+    template<typename A1, typename A2, typename A3, typename A4>
+    FixedLenParser(std::initializer_list<A1 BoxStyle::*> m1l,
+                   std::initializer_list<A2 BoxStyle::*> m2l,
+                   std::initializer_list<A3 BoxStyle::*> m3l,
+                   std::initializer_list<A4 BoxStyle::*> m4l)
+    {
+        vector<A1 BoxStyle::*> m1s = m1l;
+        vector<A2 BoxStyle::*> m2s = m2l;
+        vector<A3 BoxStyle::*> m3s = m3l;
+        vector<A4 BoxStyle::*> m4s = m4l;
+        parser4_ = [m1s,m2s,m3s,m4s](StrRef arg1, StrRef arg2, StrRef arg3, StrRef arg4) {
+            A1 arg1typed = Converter<A1>::parse(arg1);
+            A2 arg2typed = Converter<A2>::parse(arg2);
+            A3 arg3typed = Converter<A3>::parse(arg3);
+            A4 arg4typed = Converter<A4>::parse(arg4);
+            return [m1s,m2s,m3s,m4s,arg1typed,arg2typed,arg3typed,arg4typed](BoxStyle& style) -> void{
+                for (auto m1 : m1s) {
+                    style.*m1 = arg1typed;
+                }
+                for (auto m2 : m2s) {
+                    style.*m2 = arg2typed;
+                }
+                for (auto m3 : m3s) {
+                    style.*m3 = arg3typed;
+                }
+                for (auto m4 : m4s) {
+                    style.*m4 = arg4typed;
+                }
+            };
+        };
+    }
+
+};
+
+template<typename T>
+inline vector<pair<string, FixedLenParser>>
+fourSidesParser(string nameAll,
+                string nameTop, T BoxStyle::*memberTop,
+                string nameRight, T BoxStyle::*memberRight,
+                string nameBottom, T BoxStyle::*memberBottom,
+                string nameLeft, T BoxStyle::*memberLeft) {
+
+    return {
+        {nameTop, memberTop},
+        {nameRight, memberRight },
+        {nameBottom, memberBottom },
+        {nameLeft, memberLeft },
+        {nameAll, // setting all 4 sides to the same value
+           {{memberTop, memberRight, memberBottom, memberLeft }}},
+        {nameAll, // top&bot  left&right
+           {{memberTop, memberBottom}, {memberLeft, memberRight }}},
+        {nameAll, // setting all separately in one line
+           {{memberTop}, {memberRight}, {memberBottom}, {memberLeft}}},
+    };
+}
+
+static void append_vector(vector<pair<string, FixedLenParser>>& target,
+                          const vector<pair<string, FixedLenParser>>& source) {
+    target.insert(target.end(), source.cbegin(), source.cend());
+}
+
+void CssValueParser::buildParserCache()
+{
+    vector<pair<string, FixedLenParser>> memberParsers = {
+        {"display", &BoxStyle::display },
+        {"border-style", &BoxStyle::borderStyle },
+        {"border", {
+            {&BoxStyle::borderWidthTop, &BoxStyle::borderWidthRight,
+             &BoxStyle::borderWidthBottom, &BoxStyle::borderWidthLeft},
+            {&BoxStyle::borderStyle},
+            {&BoxStyle::borderColorTop, &BoxStyle::borderColorRight,
+             &BoxStyle::borderColorBottom, &BoxStyle::borderColorLeft},
+        }},
+        {"border-top", {
+            {&BoxStyle::borderWidthTop},
+            {&BoxStyle::borderStyle},
+            {&BoxStyle::borderColorTop},
+        }},
+        {"border-right", {
+            {&BoxStyle::borderWidthRight},
+            {&BoxStyle::borderStyle},
+            {&BoxStyle::borderColorRight},
+        }},
+        {"border-bottom", {
+            {&BoxStyle::borderWidthBottom},
+            {&BoxStyle::borderStyle},
+            {&BoxStyle::borderColorBottom},
+        }},
+        {"border-left", {
+            {&BoxStyle::borderWidthLeft},
+            {&BoxStyle::borderStyle},
+            {&BoxStyle::borderColorLeft},
+        }},
+        {"min-height", &BoxStyle::minHeight },
+        {"min-width", &BoxStyle::minWidth },
+        {"outline-style", &BoxStyle::outlineStyle },
+        {"background-color", &BoxStyle::backgroundColor },
+        {"font", &BoxStyle::font },
+        {"color", &BoxStyle::fontColor },
+        {"text-align", &BoxStyle::textAlign },
+        {"-hlwm-text-depth", &BoxStyle::textDepth },
+        {"-hlwm-text-height", &BoxStyle::textHeight },
+    };
+    append_vector(memberParsers,
+                  fourSidesParser(
+                      "padding",
+                      "padding-top", &BoxStyle::paddingTop,
+                      "padding-right", &BoxStyle::paddingRight,
+                      "padding-bottom", &BoxStyle::paddingBottom,
+                      "padding-left", &BoxStyle::paddingLeft)
+                  );
+    append_vector(memberParsers,
+                  fourSidesParser(
+                      "margin",
+                      "margin-top", &BoxStyle::marginTop,
+                      "margin-right", &BoxStyle::marginRight,
+                      "margin-bottom", &BoxStyle::marginBottom,
+                      "margin-left", &BoxStyle::marginLeft)
+                  );
+    append_vector(memberParsers,
+                  fourSidesParser(
+                      "border-width",
+                      "border-top-width", &BoxStyle::borderWidthTop,
+                      "border-right-width", &BoxStyle::borderWidthRight,
+                      "border-bottom-width", &BoxStyle::borderWidthBottom,
+                      "border-left-width", &BoxStyle::borderWidthLeft)
+                  );
+    append_vector(memberParsers,
+                  fourSidesParser(
+                      "border-color",
+                      "border-top-color", &BoxStyle::borderColorTop,
+                      "border-right-color", &BoxStyle::borderColorRight,
+                      "border-bottom-color", &BoxStyle::borderColorBottom,
+                      "border-left-color", &BoxStyle::borderColorLeft)
+                  );
+    append_vector(memberParsers,
+                  fourSidesParser(
+                      "outline-width",
+                      "outline-top-width", &BoxStyle::outlineWidthTop,
+                      "outline-right-width", &BoxStyle::outlineWidthRight,
+                      "outline-bottom-width", &BoxStyle::outlineWidthBottom,
+                      "outline-left-width", &BoxStyle::outlineWidthLeft)
+                  );
+    append_vector(memberParsers,
+                  fourSidesParser(
+                      "outline-color",
+                      "outline-top-color", &BoxStyle::outlineColorTop,
+                      "outline-right-color", &BoxStyle::outlineColorRight,
+                      "outline-bottom-color", &BoxStyle::outlineColorBottom,
+                      "outline-left-color", &BoxStyle::outlineColorLeft)
+                  );
+
+    propName2Parser_.clear();
+    for (const auto& line : memberParsers) {
+        auto it = propName2Parser_.find(line.first);
+        if (it == propName2Parser_.end()) {
+            propName2Parser_[line.first] = CssValueParser();
+            it = propName2Parser_.find(line.first);
+            it->second.propertyName_ = line.first;
+        }
+        if (line.second.getter_) {
+            it->second.getter_ = line.second.getter_;
+        }
+        if (line.second.valuesMatch_) {
+            it->second.valuesMatch_ = line.second.valuesMatch_;
+        }
+        if (line.second.parser1_) {
+            it->second.parser1_ = line.second.parser1_;
+        }
+        if (line.second.parser2_) {
+            it->second.parser2_ = line.second.parser2_;
+        }
+        if (line.second.parser3_) {
+            it->second.parser3_ = line.second.parser3_;
+        }
+        if (line.second.parser4_) {
+            it->second.parser4_ = line.second.parser4_;
+        }
+    }
+}
+
+BoxStyle::setter CssValueParser::parse(const vector<string>& args) const
+{
+    if (parser1_ && !(parser2_ || parser3_ || parser4_)) {
+        // if this property only accepts one argument, then join the args
+        stringstream buf;
+        for (const auto& a : args) {
+            buf << a;
+        }
+        return parser1_(buf.str());
+    }
+    if (args.size() == 1 && parser1_) {
+        return parser1_(args[0]);
+    }
+    if (args.size() == 2 && parser2_) {
+        return parser2_(args[0], args[1]);
+    }
+    if (args.size() == 3 && parser3_) {
+        return parser3_(args[0], args[1], args[2]);
+    }
+    if (args.size() == 4 && parser4_) {
+        return parser4_(args[0], args[1], args[2], args[3]);
+    }
+    throw std::invalid_argument("property \""
+                                + propertyName_
+                                + "\" does not accept "
+                                + to_string(args.size())
+                                + " arguments");
+}
+
+const CssValueParser& CssValueParser::find(const string& propertyName)
+{
+    if (propName2Parser_.empty()) {
+        // built the cache
+        buildParserCache();
+    }
+    // look up the property name
+    auto it = propName2Parser_.find(propertyName);
+    if (it != propName2Parser_.end()) {
+        return it->second;
+    } else {
+        throw std::invalid_argument("No such property \"" + propertyName + "\"");
+    }
+}
+
+void CssValueParser::foreachParser(function<void (const CssValueParser&)> loopBody)
+{
+    if (propName2Parser_.empty()) {
+        // built the cache
+        buildParserCache();
+    }
+    for (const auto& it : propName2Parser_) {
+        loopBody(it.second);
+    }
+}
+
+std::map<string, string> BoxStyle::changedProperties() const
+{
+    std::map<string,string> properties;
+    BoxStyle emptyStyle = BoxStyle::empty();
+    CssValueParser::foreachParser([&](const CssValueParser& propParser) {
+        if (propParser.valuesMatch_ && propParser.getter_) {
+            if (!propParser.valuesMatch_(*this, emptyStyle)) {
+                properties[propParser.name()] = propParser.getter_(*this);
+            }
+        }
+    });
+    return properties;
+}
---
+++ src/parserutils.h
@@ -0,0 +1,298 @@
+#pragma once
+
+/** A small generic tokenizer and parser, mainly for CSS parsing.
+ *  This header must not be included in another header file.
+ */
+#include <algorithm>
+#include <cstring>
+#include <functional>
+#include <sstream>
+#include <vector>
+
+#include "either.h"
+
+class SourceStream {
+private:
+    std::string buf;
+    size_t pos = 0;
+
+public:
+    static
+    SourceStream fromString(std::string source) {
+        SourceStream ss;
+        ss.buf = source;
+        ss.pos = 0;
+        return ss;
+    }
+
+    class ErrorData {
+    public:
+        size_t line_ = 0;
+        size_t column_ = 0;
+        std::string message_;
+        std::string str() const {
+            std::stringstream ss;
+            ss << "line " << (line_ + 1)
+               << " column " << (column_ + 1)
+               << ": " << message_;
+            return ss.str();
+        }
+    };
+
+    class Error : public std::exception {
+    public:
+        ErrorData data_;
+        std::string str() const {
+            return data_.str();
+        }
+    };
+
+    //! whitespace characters
+    static constexpr auto whitespace = " \n\t\r";
+    inline static bool contains(char const* characterList, char member) {
+        while (characterList[0]) {
+            if (member == characterList[0]) {
+                return true;
+            }
+            characterList++;
+        }
+        return false;
+    }
+
+    inline bool startswith(char const* prefix) const {
+        size_t idx = 0;
+        while (prefix[idx]) {
+            if (pos + idx >= buf.size()) {
+                return false;
+            }
+            if (prefix[idx] != buf[pos + idx]) {
+                return false;
+            }
+            idx++;
+        }
+        return true;
+    }
+
+    //! try to consume the given prefix from the input stream
+    inline bool tryConsume(char const* prefix) {
+        if (startswith(prefix)) {
+            pos += strlen(prefix);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @brief skip whitespace characters and comments
+     * @return the number of whitespace characters skipped
+     */
+    size_t skipWhitespace() {
+        size_t skipCount = 0;
+        while (pos < buf.size()) {
+            if (contains(whitespace, buf[pos])) {
+                pos++;
+                skipCount++;
+            } else if (tryConsume("/*")) {
+                while (!tryConsume("*/")) {
+                    pos++;
+                    if (isEOF()) {
+                        // multiline comments may not be ended with EOF
+                        expectedButGot("*/");
+                    }
+                }
+            } else if (tryConsume("//")) {
+                while (!tryConsume("\n") && !isEOF()) {
+                    pos++;
+                }
+            } else {
+                break;
+            }
+        }
+        return skipCount;
+    }
+
+    /**
+     * @brief return the current position in the source stream
+     * @return tuple of line/column (0-indexed)
+     */
+    std::pair<size_t,size_t> sourcePosition() const {
+        size_t line = 0;
+        size_t idx = 0;
+        size_t column = pos - idx;
+        while (idx <= pos && idx < buf.size()) {
+            if (buf[idx] == '\n') {
+                line++;
+                column = pos - (idx + 1);
+            }
+            idx++;
+        }
+        return std::make_pair(line, column);
+    }
+
+    Error constructErrorObject(const char* message) {
+        auto sourcePos = sourcePosition();
+        Error err;
+        err.data_.line_ = sourcePos.first;
+        err.data_.column_ = sourcePos.second;
+        err.data_.message_ = message;
+        return err;
+    }
+
+    void raise(const char* message) {
+        throw constructErrorObject(message);
+    }
+
+    void expectedButGot(const char* expected) {
+        std::stringstream message;
+        message << "Expected " << expected << " but got ";
+        if (pos >= buf.size()) {
+            message << "EOF";
+        } else {
+            auto preview = buf.substr(pos, 20);
+            std::replace(preview.begin(), preview.end(), '\n', ' '); // replace all '\n' with ' '
+            message << "\"" << preview << "...\"";
+        }
+        raise(message.str().c_str());
+    }
+
+    inline void consumeOrException(char const* prefix) {
+        if (!tryConsume(prefix)) {
+            expectedButGot(prefix);
+        }
+    }
+
+    bool isEOF() {
+        return pos >= buf.size();
+    }
+
+    /**
+     * @brief return the next token, which consists of either a single special character
+     * or a sequence of non-special characters
+     * @param special the special characters
+     * @return
+     */
+    std::string nextToken(const char* special) {
+        size_t token_pos = pos;
+        while (pos < buf.size()) {
+            if (token_pos == pos && contains(special, buf[pos])) {
+                // if the token starts with a special character,
+                // then the resulting token only contains this character
+                pos++;
+                break;
+            }
+            if (contains(special, buf[pos]) || contains(whitespace, buf[pos])) {
+                // end the token before special characters or whitespace
+                break;
+            }
+            // otherwise, include the current character in the token.
+            pos++;
+        }
+        std::string token = buf.substr(token_pos, pos - token_pos);
+        if (token.empty()) {
+            expectedButGot("any kind of token");
+        }
+        return token;
+    }
+};
+
+template<typename Result>
+class Parser {
+public:
+    Parser() {}
+    Parser(const std::function<Result(SourceStream& source)>& r) : run_(r) {}
+
+    bool isApplicable(SourceStream& source) const {
+        if (source.isEOF()) {
+            return false;
+        }
+        if (isApplicable_) {
+            return isApplicable_(source);
+        }
+        return true;
+    }
+    Result operator()(SourceStream& source) const {
+        return run_(source);
+    }
+
+    Either<SourceStream::ErrorData, Result> oneShot(const std::string& source) {
+        auto stream = SourceStream::fromString(source);
+        try {
+            return run_(stream);
+        } catch (const SourceStream::Error& error) {
+            return error.data_;
+        } catch (const std::exception& exc) {
+            return stream.constructErrorObject(exc.what()).data_;
+        }
+    }
+
+
+    /**
+     * @brief construct a modified parser that is only applicable if
+     * the stream does not start with the given forbidden prefix
+     */
+    Parser<Result> notApplicableFor(std::string forbiddenPrefix) const {
+        auto oldGuard = isApplicable_;
+        auto newGuard = [oldGuard, forbiddenPrefix](SourceStream& source) {
+            if (source.startswith(forbiddenPrefix.c_str())) {
+                return false;
+            } else if (oldGuard) {
+                return oldGuard(source);
+            } else {
+                return true;
+            }
+        };
+        Parser<Result> newParser = *this;
+        newParser.isApplicable_ = newGuard;
+        return newParser;
+    }
+
+    /**
+     * @brief parser constructor: iteration with separater between and
+     * possibly after elements
+     */
+    Parser<std::vector<Result>> sepEndBy(char separator) const
+    {
+        return { [&,separator](SourceStream& source) {
+            std::vector<Result> result;
+            const char separatorStr[] = {separator, '\0'};
+            while (true) {
+                if (!this->isApplicable(source)) {
+                    break;
+                }
+                result.push_back(this->run_(source));
+                if (!source.tryConsume(separatorStr)) {
+                    break;
+                }
+                source.skipWhitespace();
+            }
+            return result;
+        }};
+    }
+    /**
+     * @brief parser constructor: iteration of this parser.
+     */
+    Parser<std::vector<Result>> many() const
+    {
+        return {
+            [&](SourceStream& source) {
+                auto& elementParser = *this;
+                std::vector<Result> result;
+                while (true) {
+                    if (!elementParser.isApplicable(source)) {
+                        break;
+                    }
+                    result.push_back(elementParser(source));
+                }
+                return result;
+        }};
+    }
+
+private:
+    //! the actual parser:
+    std::function<Result(SourceStream& source)> run_;
+    //! a function that checks whether we are allowed to run the parser
+    std::function<bool(SourceStream& source)> isApplicable_ = {};
+};
+
+
