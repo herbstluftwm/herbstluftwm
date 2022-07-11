21c77c842cecd1979ea8be95e8f88694cb4f63ee
mgutz <32134+mgutz@users.noreply.github.com>
2022-07-11 12:44:23 +0800

    feat: add show_frame_decorations if_empty to show frame placeholders

--- tests/test_frame_decorations.py
+++ tests/test_frame_decorations.py
@@ -2,16 +2,19 @@ import pytest
 
 
 @pytest.mark.parametrize("running_clients_num", [0, 1, 2])
-def test_show_frame_decorations_one_frame(hlwm, x11, running_clients, running_clients_num):
+def test_show_frame_decorations_one_frame(
+    hlwm, x11, running_clients, running_clients_num
+):
     expected_frame_count = {
-        'all': 1,
-        'focused': 1,
-        'focused_if_multiple': 1 if running_clients_num > 0 else 0,
-        'nonempty': 1 if running_clients_num > 0 else 0,
-        'if_multiple': 1 if running_clients_num > 0 else 0,
-        'none': 0,
+        "all": 1,
+        "focused": 1,
+        "focused_if_multiple": 1 if running_clients_num > 0 else 0,
+        "nonempty": 1 if running_clients_num > 0 else 0,
+        "if_multiple": 1 if running_clients_num > 0 else 0,
+        "if_empty": 0 if running_clients_num > 0 else 1,
+        "none": 0,
     }
-    for v in hlwm.complete(['set', 'show_frame_decorations']):
+    for v in hlwm.complete(["set", "show_frame_decorations"]):
         hlwm.attr.settings.show_frame_decorations = v
         assert len(x11.get_hlwm_frames(only_visible=True)) == expected_frame_count[v]
 
@@ -22,15 +25,16 @@ def test_show_frame_decorations_focus(hlwm, x11):
     layout = f"""
     (split horizontal:0.5:1 (clients max:0 {winid}) (clients max:0))
     """
-    hlwm.call(['load', layout.strip()])
+    hlwm.call(["load", layout.strip()])
     expected_frame_count = {
-        'all': 2,
-        'focused': 2,
-        'focused_if_multiple': 2,
-        'nonempty': 1,
-        'if_multiple': 2,
-        'none': 0,
+        "all": 2,
+        "focused": 2,
+        "focused_if_multiple": 2,
+        "nonempty": 1,
+        "if_multiple": 2,
+        "if_empty": 1,
+        "none": 0,
     }
-    for v in hlwm.complete(['set', 'show_frame_decorations']):
+    for v in hlwm.complete(["set", "show_frame_decorations"]):
         hlwm.attr.settings.show_frame_decorations = v
         assert len(x11.get_hlwm_frames(only_visible=True)) == expected_frame_count[v]
