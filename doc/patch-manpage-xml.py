381931795e4835975a9d8d3b8a2acec57425e9df
Florian Bruhin <me@the-compiler.org>
2024-09-20 12:00:00 +0200

    python: Use pathlib instead of os.path

--- doc/patch-manpage-xml.py
+++ doc/patch-manpage-xml.py
@@ -7,7 +7,7 @@ in docbook xml format using literal2emph.xsl
 
 import sys
 import subprocess
-import os
+import pathlib
 
 # the filename of the docbook xml is probably the first
 # command line argument that is not a flag
@@ -19,13 +19,13 @@ for arg in sys.argv[1:]:
 
 xsltproc = 'xsltproc'
 
-doc_directory = os.path.dirname(__file__)
+doc_directory = pathlib.Path(__file__).parent
 
 cmd = [
     xsltproc,
     '--output',
     filename,
-    os.path.join(doc_directory, 'literal2emph.xsl'),
+    doc_directory / 'literal2emph.xsl',
     filename
 ]
 
