9e50e0e8e678ac9c6b0f5b0be2a404e992398ab3
Florian Bruhin <me@the-compiler.org>
2026-01-17 22:06:21 +0100

    Make tag selection strategy for new monitors configurable (#1601)
    Add a new `monitors.tag_selection_strategy` attribute. Depending on what it is set to, when a new monitor is added, HLWM will either:
    
    - Pick the first tag that's currently not shown (default and existing behavior)
    - Prefer the first tag that's empty (no clients) instead
    - Enforce that only an empty tag is shown

--- tests/test_doc.py
+++ tests/test_doc.py
@@ -114,6 +114,7 @@ def types_and_shorthands():
         'ShowFrameDecorations': 'n',
         'SplitAlign': 'n',
         'LayoutAlgorithm': 'n',
+        'TagSelectionStrategy': 'n',
         'TextAlign': 'n',
         'TitleWhen': 'n',
         'font': 'f',
