3d4cd555f6c64f65346b476054e94b81e0c693df
CodeFaux <42219549+codefaux@users.noreply.github.com>
2026-04-02 11:58:02 -0700

    Add corners to floatplacement rule consequence (#1652)
    This time, I was annoyed that my messenger always spawned top-left, even though I always want it bottom-right. My monitor resolution changes based on where I am, which meant I couldn't use regular numeric rules.
    
    It seemed senseless to only implement the one corner, and so now we are here.
    
    At first my placement logic used getFloatingArea().tl() and so forth, but getFloatingArea().x is not monitor 0,0 when multiple monitors are used and target monitor origin is not 0,0.
    
    As a result, the tests I wrote didn't catch if window placement into corners would be wrong for multiple monitors, only single-monitor. I'm not sure how to correct the tests, but the code does work (verified on my own system) with such.
    
    I also used hard corners instead of snap_gap at first, but I feel snap_gap is more what I/other users might expect, so I added that to the calculations and tests.

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -731,6 +731,40 @@ def test_floatplacement_none_or_center(hlwm, floatplacement, x11):
         assert (geom.x, geom.y) == (30, 40)
 
 
+@pytest.mark.parametrize('floatplacement', ['topleft', 'topright', 'bottomleft', 'bottomright'])
+def test_floatplacement_corners(hlwm, floatplacement, x11):
+    # create sizes involving only even numbers
+    mx = 8
+    my = 9
+    hlwm.call(f'move_monitor 0 500x550+{mx}+{my}')
+    snap_gap = 5
+    hlwm.call(f'set snap_gap {snap_gap}')
+    hlwm.call('floating on')
+    hlwm.call('rule floatplacement={}'.format(floatplacement))
+    winhandle, _ = x11.create_client(geometry=(30, 40, 600, 400))
+
+    geom = x11.get_absolute_geometry(winhandle)
+    # in any case, the size is not affected
+    assert (geom.width, geom.height) == (600, 400)
+
+    if floatplacement == 'topleft':
+        window_tl = (geom.x, geom.y)
+        monitor_tl = (5 + mx, 5 + my)
+        assert window_tl == monitor_tl
+    if floatplacement == 'topright':
+        window_tr = (geom.x + 600, geom.y)
+        monitor_tr = (495 + mx, 5 + my)
+        assert window_tr == monitor_tr
+    if floatplacement == 'bottomleft':
+        window_bl = (geom.x, geom.y + 400)
+        monitor_bl = (5 + mx, 545 + my)
+        assert window_bl == monitor_bl
+    if floatplacement == 'bottomright':
+        window_br = (geom.x + 600, geom.y + 400)
+        monitor_br = (495 + mx, 545 + my)
+        assert window_br == monitor_br
+
+
 def test_floatplacement_uses_other_visible_monitor(hlwm, x11):
     hlwm.call('add othertag')
     hlwm.call('set_monitors 300x350+0+0 500x550+300+0')
